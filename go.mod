90a28102c0980de251e59d496f399eb317572599
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-04-30 20:25:12 +0900

    So...

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hiroaki-yamamoto/midas
+
+go 1.14
+
+require (
+	github.com/adshao/go-binance v0.0.0-20200414012312-338a1df204bf
+	go.uber.org/zap v1.15.0
+)
---
+++ services/watcher/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/adshao/go-binance"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+func init() {
+	var err error
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logger, err = cfg.Build()
+	if err != nil {
+		logger = zap.NewExample()
+		log.Println(
+			"[WARN]: Initialization Loggger Failed. Using Example Logger.",
+			err,
+		)
+	}
+}
+
+func tradeHandler(event *binance.WsTradeEvent) {
+	logger.Info("Trade: ", zap.Any("event", event))
+}
+
+func main() {
+	rootCtx := context.Background()
+	cli := binance.NewClient("", "")
+	infoReqCtx, cancel := context.WithTimeout(rootCtx, 10*time.Second)
+	defer cancel()
+	excInfoSrv := cli.NewExchangeInfoService()
+	excInfo, err := excInfoSrv.Do(infoReqCtx)
+	if err != nil {
+		logger.Panic("Error while reading symbol info:", zap.Error(err))
+	}
+	var wg sync.WaitGroup
+	wg.Add(len(excInfo.Symbols))
+	for _, sym := range excInfo.Symbols {
+		go func(sym string) {
+			defer wg.Done()
+			logger.Info(fmt.Sprintf("Connect: %s", sym))
+			doneC, _, err := binance.WsTradeServe(
+				sym, tradeHandler, func(err error) {
+					logger.Panic(
+						"Error while reading payload from the server:", zap.Error(err),
+					)
+				},
+			)
+			if err != nil {
+				logger.Panic("Error while connecting to the server:", zap.Error(err))
+			}
+			<-doneC
+		}(sym.Symbol)
+	}
+	wg.Wait()
+}
