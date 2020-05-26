f1f62d53ca4e99170fa42a6a93c3415bdf8861d5
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-05-26 16:49:19 +0900

    Testbed

---
+++ frontend/src/app/testbed/testbed.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TestbedRoutingModule } from './testbed-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TestbedRoutingModule
+  ]
+})
+export class TestbedModule { }
