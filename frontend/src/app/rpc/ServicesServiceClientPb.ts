bcddf673ab8e271b0529ab14e2ed6599db88c71f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-05-29 08:19:43 +0900

    Generated code from protobuf

---
+++ frontend/src/app/rpc/ServicesServiceClientPb.ts
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for services
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import {
+  BotInfoList,
+  BotInfoListRequest} from './services_pb';
+
+export class BotManagerClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'binary';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoListBotInfo = new grpcWeb.AbstractClientBase.MethodInfo(
+    BotInfoList,
+    (request: BotInfoListRequest) => {
+      return request.serializeBinary();
+    },
+    BotInfoList.deserializeBinary
+  );
+
+  listBotInfo(
+    request: BotInfoListRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: BotInfoList) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/services.BotManager/ListBotInfo',
+      request,
+      metadata || {},
+      this.methodInfoListBotInfo,
+      callback);
+  }
+
+}
+
---
+++ frontend/src/app/rpc/services_pb.d.ts
@@ -0,0 +1,77 @@
+import * as jspb from "google-protobuf"
+
+export class BotInfo extends jspb.Message {
+  getId(): string;
+  setId(value: string): void;
+
+  getStrategy(): Strategy;
+  setStrategy(value: Strategy): void;
+
+  getName(): string;
+  setName(value: string): void;
+
+  getConfig(): string;
+  setConfig(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): BotInfo.AsObject;
+  static toObject(includeInstance: boolean, msg: BotInfo): BotInfo.AsObject;
+  static serializeBinaryToWriter(message: BotInfo, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): BotInfo;
+  static deserializeBinaryFromReader(message: BotInfo, reader: jspb.BinaryReader): BotInfo;
+}
+
+export namespace BotInfo {
+  export type AsObject = {
+    id: string,
+    strategy: Strategy,
+    name: string,
+    config: string,
+  }
+}
+
+export class BotInfoList extends jspb.Message {
+  getBotsList(): Array<BotInfo>;
+  setBotsList(value: Array<BotInfo>): void;
+  clearBotsList(): void;
+  addBots(value?: BotInfo, index?: number): BotInfo;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): BotInfoList.AsObject;
+  static toObject(includeInstance: boolean, msg: BotInfoList): BotInfoList.AsObject;
+  static serializeBinaryToWriter(message: BotInfoList, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): BotInfoList;
+  static deserializeBinaryFromReader(message: BotInfoList, reader: jspb.BinaryReader): BotInfoList;
+}
+
+export namespace BotInfoList {
+  export type AsObject = {
+    botsList: Array<BotInfo.AsObject>,
+  }
+}
+
+export class BotInfoListRequest extends jspb.Message {
+  getOffset(): number;
+  setOffset(value: number): void;
+
+  getLimit(): number;
+  setLimit(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): BotInfoListRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: BotInfoListRequest): BotInfoListRequest.AsObject;
+  static serializeBinaryToWriter(message: BotInfoListRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): BotInfoListRequest;
+  static deserializeBinaryFromReader(message: BotInfoListRequest, reader: jspb.BinaryReader): BotInfoListRequest;
+}
+
+export namespace BotInfoListRequest {
+  export type AsObject = {
+    offset: number,
+    limit: number,
+  }
+}
+
+export enum Strategy { 
+  TRAILING = 0,
+}
