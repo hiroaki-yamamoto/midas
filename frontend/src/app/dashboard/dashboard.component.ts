7fc24c0a131e3413931b98abd8d656ef71494d88
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-06-01 20:11:51 +0900

    Added cursor and ledgend

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -5,8 +5,15 @@ import {
   AfterViewInit,
 } from '@angular/core';
 
-import { create } from '@amcharts/amcharts4/core';
-import { XYChart, LineSeries, DateAxis, ValueAxis } from '@amcharts/amcharts4/charts';
+import { create, color } from '@amcharts/amcharts4/core';
+import {
+  XYChart,
+  XYCursor,
+  LineSeries,
+  DateAxis,
+  ValueAxis,
+  Legend,
+} from '@amcharts/amcharts4/charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,6 +25,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   constructor(private zone: NgZone) { }
 
   ngAfterViewInit() {
+    const txtColor = color('#ffffff');
     this.zone.runOutsideAngular(() => {
       const g = create('compare-graph', XYChart);
       g.data = [];
@@ -26,10 +34,15 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         time.setDate(time.getDate() - i);
         g.data.push({
           date: time,
-          hodl: Math.cos(i / 12),
-          bot: Math.sin(i / 12),
+          hodl: Math.cos(i / 12) * 100,
+          bot: Math.sin(i / 12) * 100,
         });
       }
+
+      g.cursor = new XYCursor();
+      g.cursor.lineY.opacity = 0;
+      g.legend = new Legend();
+      g.legend.labels.template.fill = txtColor;
 
       // Create axes
       const dateAxis = g.xAxes.push(new DateAxis());
@@ -37,21 +50,32 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       dateAxis.renderer.grid.template.location = 0.5;
       dateAxis.startLocation = 0.5;
       dateAxis.endLocation = 0.5;
+      dateAxis.renderer.grid.template.stroke = txtColor;
+      dateAxis.renderer.labels.template.fill = txtColor;
+      dateAxis.tooltipDateFormat = 'd MMMM';
 
       // Create value axis
       const valueAxis = g.yAxes.push(new ValueAxis());
+      valueAxis.renderer.grid.template.stroke = txtColor;
+      valueAxis.renderer.labels.template.fill = txtColor;
 
       const hodl = g.series.push(new LineSeries());
       hodl.dataFields.dateX = 'date';
       hodl.dataFields.valueY = 'hodl';
       hodl.strokeWidth = 1;
       hodl.tensionX = 0.5;
+      hodl.fillOpacity = 0.4;
+      hodl.name = 'Hodl BTC Profit';
+      hodl.tooltipText = 'Hodl BTC Profit Ratio: [bold]{hodl}%[/]';
 
       const bot = g.series.push(new LineSeries());
       bot.dataFields.dateX = 'date';
       bot.dataFields.valueY = 'bot';
       bot.strokeWidth = 1;
       bot.tensionX = 0.5;
+      bot.fillOpacity = 0.4;
+      bot.name = 'Bot Trading Profit';
+      bot.tooltipText = 'Bot Trading Profit Ratio: [bold]{bot}%[/]';
     });
   }
 
