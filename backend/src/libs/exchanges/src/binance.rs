1bd3c6c3fb6d1b58a09b64a6af39226cca16883b
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-07-14 10:36:18 +0900

    Initialized HistQuery

--- backend/src/libs/exchanges/src/binance.rs
+++ backend/src/libs/exchanges/src/binance.rs
@@ -1,6 +1,34 @@
+use ::chrono::{DateTime, Utc};
+use ::serde::Serialize;
+use ::std::error::Error;
 use ::types::ParseURLResult;
+use ::url::Url;
 
 use crate::traits::Exchange;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct HistQuery {
+  pub symbol: String,
+  pub interval: String,
+  pub start_time: String,
+  pub end_time: String,
+  pub limit: String,
+}
+
+struct Kline {
+  pub open_time: DateTime<Utc>,
+  pub open_price: f64,
+  pub high_price: f64,
+  pub low_price: f64,
+  pub close_price: f64,
+  pub volume: f64,
+  pub close_time: DateTime<Utc>,
+  pub quote_volume: f64,
+  pub num_trades: i64,
+  pub taker_buy_base_volume: f64,
+  pub taker_buy_quote_volume: f64
+}
 
 pub struct Binance;
 
@@ -10,6 +38,27 @@ impl Binance {
   }
   fn get_rest_endpoint(&self) -> ParseURLResult {
     return "https://api.binance.com".parse();
+  }
+  async fn get_hist(
+    self,
+    pair: String,
+    startAt: DateTime<Utc>,
+    endAt: DateTime<Utc>,
+  ) -> Result<Vec<Kline>, Box<dyn Error>> {
+    let mut limit = (endAt - startAt).num_minutes();
+    if limit <= 1000 {
+      limit = 1000;
+    }
+    let url = self.get_rest_endpoint()?;
+    let url = url.join("/api/v3/klines")?;
+    let param = HistQuery{
+      symbol: pair,
+      interval: String::from("1m"),
+      start_time: format!("{}",startAt.timestamp()),
+      end_time: format!("{}", endAt.timestamp()),
+      limit: format!("{}", limit),
+    };
+    return Ok(vec![Kline{}]);
   }
 }
 
