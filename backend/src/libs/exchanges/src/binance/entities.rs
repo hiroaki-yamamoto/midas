5be6981eed4bba8216ce2e9ed06d2ef29d98926f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-07-27 11:31:12 +0900

    Added symbol info entity

--- backend/src/libs/exchanges/src/binance/entities.rs
+++ backend/src/libs/exchanges/src/binance/entities.rs
@@ -1,5 +1,5 @@
 use ::chrono::{DateTime, Utc};
-use ::serde::Serialize;
+use ::serde::{Serialize, Deserialize};
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -9,6 +9,72 @@ pub(crate) struct HistQuery {
   pub start_time: String,
   pub end_time: String,
   pub limit: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Symbol {
+    pub symbol: String,
+    pub status: String,
+    pub base_asset: String,
+    pub base_asset_precision: u64,
+    pub base_commission_precision: u64,
+    pub quote_commission_precision: u64,
+    pub quote_asset: String,
+    pub quote_precision: u64,
+    pub order_types: Vec<String>,
+    pub oco_allowed: bool,
+    pub iceberg_allowed: bool,
+    pub quote_order_qty_market_allowed: bool,
+    pub is_spot_trading_allowed: bool,
+    pub is_margin_trading_allowed: bool,
+    pub filters: Vec<Filters>,
+    pub permissions: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "filterType")]
+pub enum Filters {
+    #[serde(rename = "PRICE_FILTER", rename_all = "camelCase")]
+    PriceFilter {
+        min_price: String,
+        max_price: String,
+        tick_size: String,
+    },
+    #[serde(rename = "PERCENT_PRICE", rename_all = "camelCase")]
+    PercentPrice {
+        multiplier_up: String,
+        multiplier_down: String,
+        avg_price_mins: Option<f64>,
+    },
+    #[serde(rename = "LOT_SIZE", rename_all = "camelCase")]
+    LotSize {
+        min_qty: String,
+        max_qty: String,
+        step_size: String,
+    },
+    #[serde(rename = "MIN_NOTIONAL", rename_all = "camelCase")]
+    MinNotional {
+        min_notional: String,
+        apply_to_market: bool,
+        avg_price_mins: Option<f64>,
+    },
+    #[serde(rename = "ICEBERG_PARTS", rename_all = "camelCase")]
+    IcebergParts { limit: Option<u16> },
+    #[serde(rename = "MAX_NUM_ORDERS", rename_all = "camelCase")]
+    MaxNumOrders { limit: Option<u16> },
+    #[serde(rename = "MAX_NUM_ALGO_ORDERS", rename_all = "camelCase")]
+    MaxNumAlgoOrders { max_num_algo_orders: Option<u16> },
+    #[serde(rename = "MAX_NUM_ICEBERG_ORDERS", rename_all = "camelCase")]
+    MaxNumIcebergOrders { max_num_iceberg_orders: u16 },
+    #[serde(rename = "MAX_POSITION", rename_all = "camelCase")]
+    MaxPosition { max_position: String },
+    #[serde(rename = "MARKET_LOT_SIZE", rename_all = "camelCase")]
+    MarketLotSize {
+        min_qty: String,
+        max_qty: String,
+        step_size: String,
+    },
 }
 
 pub struct HistFetcherParam {
