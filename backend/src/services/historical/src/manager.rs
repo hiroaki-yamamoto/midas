fe31c9b94da81a497654fb8495b7b1b57f8847c2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-08-21 17:53:33 +0900

    Record progress to hist_fetch_prog hashmap

--- backend/src/services/historical/src/manager.rs
+++ backend/src/services/historical/src/manager.rs
@@ -5,6 +5,7 @@ use ::tokio::sync::{broadcast, mpsc};
 
 use ::exchanges::Exchange;
 use ::rpc::historical::HistChartProg;
+use ::slog::{error, o, Logger};
 use ::types::SendableErrorResult;
 
 const CHAN_BUF_SIZE: usize = 1024;
@@ -12,24 +13,26 @@ const CHAN_BUF_SIZE: usize = 1024;
 #[derive(Debug)]
 pub(crate) struct ExchangeManager<T>
 where
-  T: Exchange,
+  T: Exchange + Send,
 {
   pub exchange: T,
   pub hist_fetch_prog: HashMap<String, HistChartProg>,
+  logger: Logger,
 }
 
 impl<T> ExchangeManager<T>
 where
-  T: Exchange,
+  T: Exchange + Send,
 {
-  pub fn new(exchange: T) -> Self {
+  pub fn new(exchange: T, logger: Logger) -> Self {
     return Self {
       exchange,
       hist_fetch_prog: HashMap::new(),
+      logger,
     };
   }
   pub async fn refresh_historical_klines(
-    &mut self,
+    &'static mut self,
     symbols: Vec<String>,
   ) -> SendableErrorResult<(
     broadcast::Sender<()>,
@@ -38,14 +41,37 @@ where
     let (stop, mut prog) = self.exchange.refresh_historical(symbols).await?;
     let (mut ret_send, ret_recv) = mpsc::channel(CHAN_BUF_SIZE);
     let mut stop_recv = stop.subscribe();
+    let logger_in_thread = self
+      .logger
+      .new(o!("scope" => "refresh_historical_klines.thread"));
     thread::spawn(move || {
       ::tokio::spawn(async move {
         while let Err(_) = stop_recv.try_recv() {
           let prog = match prog.recv().await {
             None => break,
-            Some(v) => v,
+            Some(v) => match v {
+              Err(e) => {
+                error!(
+                  logger_in_thread,
+                  "Got an error when getting progress: {}", e
+                );
+                continue;
+              }
+              Ok(k) => k,
+            },
           };
-          ret_send.send(prog);
+          match self.hist_fetch_prog.get_mut(&prog.symbol) {
+            None => {
+              self
+                .hist_fetch_prog
+                .insert(prog.symbol.clone(), prog.clone());
+            }
+            Some(v) => {
+              v.cur_symbol_num += prog.cur_symbol_num;
+              v.cur_object_num += prog.cur_object_num;
+            }
+          };
+          ret_send.send(Ok(prog));
         }
       });
     });
