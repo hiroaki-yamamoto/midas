cef6341df43732bbe62f72bd323ad380b137408d
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-08-29 11:27:38 +0900

    Trying to make statically-linked binary

--- backend/devel.dockerfile
+++ backend/devel.dockerfile
@@ -1,6 +1,7 @@
-FROM golang:alpine
+FROM rust:alpine
 
 ARG SERVICE
 ENV SERVICE=${SERVICE}
+WORKDIR /opt/code
 
 ENTRYPOINT [ "./run.sh" ]
--- backend/run.sh
+++ backend/run.sh
@@ -3,6 +3,9 @@
 
 set -e
 
-go mod download -x
-go build -o /bin/app ./services/${SERVICE}/main.go
-exec app $@
+LIBRARY_PATH=/usr/local/rustup/toolchains/1.46.0-x86_64-unknown-linux-musl/lib/rustlib/x86_64-unknown-linux-musl/lib/self-contained:${LIBRARY_PATH}
+CPATH=/usr/lib/gcc/x86_64-alpine-linux-musl/9.3.0/include:${CPATH}
+export LIBRARY_PATH CPATH
+
+cargo build -p ${SERVICE}
+exec ./target/debug/${SERVICE} $@
