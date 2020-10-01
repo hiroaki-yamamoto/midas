5e989464f4fa494338e075a1389ee8db90164b96
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-10-01 16:44:38 +0900

    Implemented Historical Fetch Worker

--- backend/libs/exchanges/src/binance/history_fetcher.rs
+++ backend/libs/exchanges/src/binance/history_fetcher.rs
@@ -355,78 +355,76 @@ impl HistoryFetcherTrait for HistoryFetcher {
         .await
     )
     .map(|item| (from_msgpack::<HistFetcherParam>(item.data.as_ref()), item));
-    ::tokio::spawn(async move {
-      loop {
-        select! {
-          Some((param, msg)) = param_sub.next() => {
-            let param = match param {
-              Err(e) => {
-                warn!(me.logger, "Failed to parse the param msg: {}", e);
-                continue;
-              },
-              Ok(v) => v
-            };
-            let num_obj = match param.end_time {
-              None => 1,
-              Some(end_time) => (*end_time - *param.start_time).num_minutes()
-            };
-            if num_obj > NUM_OBJECTS_TO_FETCH as i64 {
+    loop {
+      select! {
+        Some((param, msg)) = param_sub.next() => {
+          let param = match param {
+            Err(e) => {
+              warn!(me.logger, "Failed to parse the param msg: {}", e);
+              continue;
+            },
+            Ok(v) => v
+          };
+          let num_obj = match param.end_time {
+            None => 1,
+            Some(end_time) => (*end_time - *param.start_time).num_minutes()
+          };
+          if num_obj > NUM_OBJECTS_TO_FETCH as i64 {
+            warn!(
+              me.logger,
+              "Duration between the specified start and end time exceeds
+                the maximum number of the objects to fetch.";
+              "symbol" => &param.symbol,
+              "start_time" => param.start_time.to_string(),
+              "end_time" => format!("{:?}", param.end_time),
+              "num_objects" => num_obj,
+              "maximum_number_objects" => NUM_OBJECTS_TO_FETCH,
+            );
+            continue;
+          }
+          let start_time = *param.start_time;
+          let resp = me.fetch(
+            param.symbol.clone(),
+            start_time,
+            param.end_time.map(|d| *d),
+          );
+          let resp = resp.await;
+          let resp = match resp {
+            Err(e) => {
+              warn!(me.logger, "Failed to fetch kline data: {}", e);
+              continue;
+            },
+            Ok(v) => v
+          };
+          let response_payload = match to_msgpack(KlinesWithInfo{
+            klines: resp,
+            symbol: param.symbol.clone(),
+            num_symbols: param.num_symbols,
+            entire_data_len: param.entire_data_len
+          }.as_ref()) {
+            Err(e) => {
               warn!(
                 me.logger,
-                "Duration between the specified start and end time exceeds
-                  the maximum number of the objects to fetch.";
-                "symbol" => &param.symbol,
-                "start_time" => param.start_time.to_string(),
-                "end_time" => format!("{:?}", param.end_time),
-                "num_objects" => num_obj,
-                "maximum_number_objects" => NUM_OBJECTS_TO_FETCH,
+                "Failed to serialize the payload for response: {}", e
               );
               continue;
-            }
-            let start_time = *param.start_time;
-            let resp = me.fetch(
-              param.symbol.clone(),
-              start_time,
-              param.end_time.map(|d| *d),
-            );
-            let resp = resp.await;
-            let resp = match resp {
-              Err(e) => {
-                warn!(me.logger, "Failed to fetch kline data: {}", e);
-                continue;
-              },
-              Ok(v) => v
-            };
-            let response_payload = match to_msgpack(KlinesWithInfo{
-              klines: resp,
-              symbol: param.symbol.clone(),
-              num_symbols: param.num_symbols,
-              entire_data_len: param.entire_data_len
-            }.as_ref()) {
-              Err(e) => {
-                warn!(
-                  me.logger,
-                  "Failed to serialize the payload for response: {}", e
-                );
-                return;
-              },
-              Ok(v) => v
-            };
-            match msg.reply {
-              Some(_) => {
-                let _ = msg.respond(response_payload.as_slice().to_owned()).await;
-              },
-              None => {
-                let _ = me.broker.publish(
-                  HIST_FETCHER_FETCH_RESP_SUB_NAME,
-                  response_payload.as_slice().to_owned()).await;
-              },
-            };
-          },
-          else => {break;}
-        }
+            },
+            Ok(v) => v
+          };
+          match msg.reply {
+            Some(_) => {
+              let _ = msg.respond(response_payload.as_slice().to_owned()).await;
+            },
+            None => {
+              let _ = me.broker.publish(
+                HIST_FETCHER_FETCH_RESP_SUB_NAME,
+                response_payload.as_slice().to_owned()).await;
+            },
+          };
+        },
+        else => {break;}
       }
-    });
+    }
     return Ok(());
   }
 
--- backend/workers/historical/src/historical_fetcher.rs
+++ backend/workers/historical/src/historical_fetcher.rs
@@ -1,11 +1,17 @@
 use ::clap::Clap;
+use ::futures::future::{join_all, select};
+use ::libc::{SIGINT, SIGTERM};
 use ::mongodb::options::ClientOptions as MongoDBCliOpt;
 use ::mongodb::Client as DBCli;
 use ::nats::asynk::connect;
-use ::slog::{info, Logger};
+use ::slog::{info, o, Logger};
+use ::tokio::signal::unix as signal;
 
 use ::config::{CmdArgs, Config};
-use ::exchanges::binance::HistoryFetcher as BinanceHistoryFetcher;
+use ::exchanges::binance::{
+  HistoryFetcher as BinanceHistoryFetcher,
+  SymbolFetcher as BinanceSymbolFetcher,
+};
 use ::exchanges::HistoryFetcher;
 use ::slog_builder::{build_debug, build_json};
 use ::types::GenericResult;
@@ -23,17 +29,21 @@ async fn main() -> GenericResult<()> {
     logger = prd_logger;
   }
   info!(logger, "Kline fetch worker");
-  let broker = connect(&cfg.broker_url).await;
+  let broker = connect(&cfg.broker_url).await?;
   let db = DBCli::with_options(MongoDBCliOpt::parse(&cfg.db_url).await?)?
     .database("midas");
-  // let fetchers: Vec<Box<dyn HistoryFetcher>> = vec![Box::new(
-  //   BinanceHistoryFetcher::new(
-  //     None,
-  //     logger,
-  //     broker,
-  //     symbol_fetcher: SymbolFetcher,
-  //   )
-  //   .await?,
-  // )];
+  let fetchers: Vec<Box<dyn HistoryFetcher>> = vec![Box::new(
+    BinanceHistoryFetcher::new(
+      None,
+      logger.clone(),
+      broker,
+      BinanceSymbolFetcher::new(logger.new(o!("scope" => "SymbolFetcher")), db),
+    )
+    .await?,
+  )];
+  let fetchers = fetchers.iter().map(|fetcher| fetcher.spawn());
+  let mut sig = signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT))?;
+  let sig = Box::pin(sig.recv());
+  select(join_all(fetchers), sig).await;
   return Ok(());
 }
