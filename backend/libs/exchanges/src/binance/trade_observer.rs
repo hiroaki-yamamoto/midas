74381ed50811e1b7515679c0a798be9b15e38e59
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-10-10 13:59:21 +0900

    Implemented symbol update handle event for trade observer

--- backend/libs/exchanges/src/binance/trade_observer.rs
+++ backend/libs/exchanges/src/binance/trade_observer.rs
@@ -29,6 +29,7 @@ use super::entities::{
 use crate::errors::WebsocketError;
 use crate::traits::TradeObserver as TradeObserverTrait;
 
+#[derive(Clone)]
 pub struct TradeObserver {
   id: u32,
   broker: Broker,
@@ -87,7 +88,7 @@ impl TradeObserver {
     for symbol in &self.symbols {
       inner
         .params
-        .push(format!("{}@{}", symbol.to_lowercase(), "trade"));
+        .push(format!("{}@trade", symbol.to_lowercase()));
     }
     let req = TradeSubRequest::Subscribe(inner);
     let payload = ret_on_err!(to_json(&req));
@@ -98,7 +99,44 @@ impl TradeObserver {
     return Ok(());
   }
 
-  async fn handle_event(&self) -> SendableErrorResult<()> {
+  async fn update_symbols(
+    &mut self,
+    socket: &mut WebSocketStream<TcpStream>,
+    update_event: SymbolUpdateEvent,
+  ) -> SendableErrorResult<()> {
+    let mut sub_req_inner = TradeSubRequestInner {
+      id: self.id,
+      params: vec![],
+    };
+    let mut unsub_req_inner = sub_req_inner.clone();
+    for to_sub in &update_event.to_add {
+      sub_req_inner
+        .params
+        .push(format!("{}@trade", to_sub.to_lowercase()));
+    }
+    self.symbols.extend(update_event.to_add);
+    for to_unsub in &update_event.to_remove {
+      let sub_name = format!("{}@trade", to_unsub.to_lowercase());
+      unsub_req_inner.params.push(sub_name.clone());
+      let sym_position =
+        self.symbols.iter().position(move |name| name == &sub_name);
+      if let Some(pos) = sym_position {
+        self.symbols.remove(pos);
+      }
+    }
+    let sub_req = TradeSubRequest::Subscribe(sub_req_inner);
+    let unsub_req = TradeSubRequest::Unsubscribe(unsub_req_inner);
+    let sub_payload = ret_on_err!(to_json(&sub_req));
+    let unsub_payload = ret_on_err!(to_json(&unsub_req));
+    let _ = socket.send(wsocket::Message::Binary(unsub_payload)).await;
+    let _ = socket.send(wsocket::Message::Binary(sub_payload)).await;
+    return Ok(());
+  }
+
+  async fn handle_event(
+    &mut self,
+    socket: &mut WebSocketStream<TcpStream>,
+  ) -> SendableErrorResult<()> {
     let mut symbol_update =
       ret_on_err!(self.broker.subscribe(SYMBOL_UPDATE_EVENT).await);
     loop {
@@ -111,6 +149,9 @@ impl TradeObserver {
             },
             Ok(o) => o
           };
+          if let Err(e) = self.update_symbols(socket, event).await {
+            ::slog::warn!(self.logger, "Got an error while updating symbol: {}", e);
+          }
         },
         else => {break;}
       }
@@ -122,8 +163,18 @@ impl TradeObserver {
 #[async_trait]
 impl TradeObserverTrait for TradeObserver {
   async fn start(&self) -> SendableErrorResult<NatsSub> {
-    let mut socket = self.init_socket().await?;
-    self.init_subscription(&mut socket).await?;
+    let mut me = self.clone();
+    let mut socket = me.init_socket().await?;
+    me.init_subscription(&mut socket).await?;
+    ::tokio::spawn(async move {
+      if let Err(e) = me.handle_event(&mut socket).await {
+        ::slog::error!(
+          me.logger,
+          "Failed to open the handler of the trade event: {}",
+          e,
+        );
+      };
+    });
     let sub = ret_on_err!(self.broker.subscribe(TRADE_OBSERVER_SUB_NAME).await);
     return Ok(sub);
   }
