9242d3075218b6a17e97b54838fc3b32d1e20ca8
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-11-09 17:44:43 +0900

    Adding the route

--- backend/services/observer/src/main.rs
+++ backend/services/observer/src/main.rs
@@ -1,4 +1,6 @@
 use ::clap::Clap;
+use ::futures::{FutureExt, SinkExt};
+use ::warp::Filter;
 
 use ::config::{CmdArgs, Config};
 use ::exchanges::binance;
@@ -7,5 +9,10 @@ fn main() {
   let cmd: CmdArgs = CmdArgs::parse();
   let cfg = Config::from_fpath(Some(cmd.config)).unwrap();
   let (logger, _) = cfg.build_slog();
+  let route = warp::path::param::<String>().and(::warp::ws()).map(
+    |exchange: String, ws: ::warp::ws::Ws| {
+      return ws.on_upgrade(|socket: ::warp::ws::WebSocket| async {});
+    },
+  );
   println!("Hello, world!");
 }
