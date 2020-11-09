211c0a53fce96859bb32132c1f8cb360867523e4
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-11-09 16:53:49 +0900

    Adding observer service to watch book ticker

--- backend/services/observer/src/main.rs
+++ backend/services/observer/src/main.rs
@@ -1,5 +1,11 @@
+use ::clap::Clap;
+
+use ::config::{CmdArgs, Config};
 use ::exchanges::binance;
 
 fn main() {
+  let cmd: CmdArgs = CmdArgs::parse();
+  let cfg = Config::from_fpath(Some(cmd.config)).unwrap();
+  let (logger, _) = cfg.build_slog();
   println!("Hello, world!");
 }
