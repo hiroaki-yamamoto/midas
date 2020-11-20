fe508ca8a0eb2f4b0c29441761cfcda173c9549f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-11-21 08:29:25 +0900

    Implemented protect function, and now... implementing cookie token generation

--- backend/libs/csrf/src/lib.rs
+++ backend/libs/csrf/src/lib.rs
@@ -7,22 +7,15 @@ pub use ::http::Method;
 
 #[derive(Debug, Clone)]
 pub struct CSRFOption {
-  cookie_name: String,
-  header_name: String,
-  verify_methods: Vec<Method>,
+  cookie_name: &'static str,
+  header_name: &'static str,
 }
 
 impl Default for CSRFOption {
   fn default() -> Self {
     return Self {
-      cookie_name: "XSRF-TOKEN".to_string(),
-      header_name: "X-XSRF-TOKEN".to_string(),
-      verify_methods: vec![
-        Method::POST,
-        Method::DELETE,
-        Method::PUT,
-        Method::PATCH,
-      ],
+      cookie_name: "XSRF-TOKEN",
+      header_name: "X-XSRF-TOKEN",
     };
   }
 }
@@ -31,16 +24,12 @@ impl CSRFOption {
   pub fn builder() -> Self {
     return Self::default();
   }
-  pub fn cookie_name(mut self, cookie_name: String) -> Self {
+  pub fn cookie_name(mut self, cookie_name: &'static str) -> Self {
     self.cookie_name = cookie_name;
     return self;
   }
-  pub fn header_name(mut self, header_name: String) -> Self {
+  pub fn header_name(mut self, header_name: &'static str) -> Self {
     self.header_name = header_name;
-    return self;
-  }
-  pub fn verify_methods(mut self, verify_methods: Vec<Method>) -> Self {
-    self.verify_methods = verify_methods;
     return self;
   }
 }
@@ -54,39 +43,18 @@ impl CSRF {
   pub fn new(opt: CSRFOption) -> Self {
     return Self { opt };
   }
-  pub fn protect<F, R1>(
+  pub fn protect(
     &self,
-    filter: F,
-  ) -> impl Filter<Extract = (R1,), Error = ::warp::Rejection>
+  ) -> impl Filter<Extract = (), Error = ::warp::Rejection>
        + Clone
        + Send
        + Sync
-       + 'static
-  where
-    F: Filter<Extract = (R1,), Error = ::std::convert::Infallible>
-      + Clone
-      + Send
-      + Sync
-      + 'static,
-    F::Extract: Reply,
-  {
-    let cookie_name = self.opt.cookie_name;
-    let header_name = self.opt.header_name;
-    let methods = self.opt.verify_methods;
-
-    return ::warp::method()
-      .and(::warp::filters::cookie::optional(
-        &cookie_name,
-      ))
-      .and(::warp::filters::header::optional(
-        &header_name,
-      ))
+       + 'static {
+    return ::warp::any()
+      .and(::warp::filters::cookie::optional(self.opt.cookie_name))
+      .and(::warp::filters::header::optional(self.opt.header_name))
       .and_then(
-        |method: Method, cookie: Option<String>, header: Option<String>| async move {
-          let verify_methods = methods.clone();
-          if !methods.contains(&method) {
-            return Ok(());
-          }
+        |cookie: Option<String>, header: Option<String>| async move {
           if cookie.is_none() || header.is_none() {
             return Err(::warp::reject::custom(CSRFCheckFailed::new(
               "Either cookie or header is none.".to_string(),
@@ -105,7 +73,25 @@ impl CSRF {
           )));
         },
       )
-      .untuple_one()
-      .and(filter);
+      .untuple_one();
+  }
+
+  pub fn generate_cookie<F, Resp>(
+    &self,
+    filter: F,
+  ) -> impl Filter<Extract = (impl Reply,), Error = ::std::convert::Infallible>
+       + Clone
+       + Send
+       + Sync
+       + 'static
+  where
+    F: Filter<Extract = (Resp,), Error = ::std::convert::Infallible>
+      + Clone
+      + Send
+      + Sync
+      + 'static,
+    Resp: Reply,
+  {
+    return ::warp::any().and(filter).map(|reply: Resp| {});
   }
 }
