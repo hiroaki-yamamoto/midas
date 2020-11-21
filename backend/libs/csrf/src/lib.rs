3fce1d9eb160b02b41bbf3876be29162f0f22760
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-11-21 14:50:29 +0900

    Implemented cookie token generator

--- backend/libs/csrf/src/lib.rs
+++ backend/libs/csrf/src/lib.rs
@@ -1,9 +1,14 @@
 mod errors;
 
+use ::cookie::CookieBuilder;
+use ::hyper::header::SET_COOKIE;
+use ::rand::distributions::Alphanumeric;
+use ::rand::{thread_rng, Rng};
+use ::time::Duration as TimeDuration;
+use ::warp::reply;
 use ::warp::{Filter, Reply};
 
 use self::errors::CSRFCheckFailed;
-pub use ::http::Method;
 
 #[derive(Debug, Clone)]
 pub struct CSRFOption {
@@ -53,33 +58,31 @@ impl CSRF {
     return ::warp::any()
       .and(::warp::filters::cookie::optional(self.opt.cookie_name))
       .and(::warp::filters::header::optional(self.opt.header_name))
-      .and_then(
-        |cookie: Option<String>, header: Option<String>| async move {
-          if cookie.is_none() || header.is_none() {
-            return Err(::warp::reject::custom(CSRFCheckFailed::new(
-              "Either cookie or header is none.".to_string(),
-              format!("{:?}", cookie),
-              format!("{:?}", header),
-            )));
-          }
-          let (cookie, header) = (cookie.unwrap(), header.unwrap());
-          if cookie == header {
-            return Ok(());
-          }
+      .and_then(|cookie: Option<String>, header: Option<String>| async {
+        if cookie.is_none() || header.is_none() {
           return Err(::warp::reject::custom(CSRFCheckFailed::new(
-            "CSRF Token Mismatch".to_string(),
-            cookie,
-            header,
+            "Either cookie or header is none.".to_string(),
+            format!("{:?}", cookie),
+            format!("{:?}", header),
           )));
-        },
-      )
+        }
+        let (cookie, header) = (cookie.unwrap(), header.unwrap());
+        if cookie == header {
+          return Ok(());
+        }
+        return Err(::warp::reject::custom(CSRFCheckFailed::new(
+          "CSRF Token Mismatch".to_string(),
+          cookie,
+          header,
+        )));
+      })
       .untuple_one();
   }
 
   pub fn generate_cookie<F, Resp>(
     &self,
     filter: F,
-  ) -> impl Filter<Extract = (impl Reply,), Error = ::std::convert::Infallible>
+  ) -> impl Filter<Extract = (reply::Response,), Error = ::std::convert::Infallible>
        + Clone
        + Send
        + Sync
@@ -92,6 +95,27 @@ impl CSRF {
       + 'static,
     Resp: Reply,
   {
-    return ::warp::any().and(filter).map(|reply: Resp| {});
+    let cookie_name = self.opt.cookie_name.clone();
+    return ::warp::cookie::optional(&cookie_name).and(filter).map(
+      move |req_cookie: Option<String>, resp: Resp| {
+        let value: String =
+          thread_rng().sample_iter(&Alphanumeric).take(50).collect();
+        let cookie = CookieBuilder::new(cookie_name, value)
+          .max_age(TimeDuration::new(3600, 0))
+          .secure(true)
+          .finish();
+        match req_cookie {
+          None => {
+            return reply::with_header(
+              resp,
+              SET_COOKIE.as_str(),
+              cookie.to_string(),
+            )
+            .into_response();
+          }
+          Some(_) => return resp.into_response(),
+        };
+      },
+    );
   }
 }
