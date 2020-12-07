773ca0bfdd91c6d65cbe3091c4b4ba2687048ab1
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-12-07 16:25:59 +0900

    Trying to implement front test executor, but I had a lot of things to do today...

--- backend/libs/exchanges/src/binance/executors/front_test.rs
+++ backend/libs/exchanges/src/binance/executors/front_test.rs
@@ -1,5 +1,41 @@
+use ::std::collections::HashMap;
+
+use ::mongodb::bson::oid::ObjectId;
 use ::nats::asynk::Connection as NatsCon;
 
+use crate::traits::Executor as ExecutorTrait;
+
+use super::entities::{Order, OrderInner};
+
 pub struct Executor {
-  con: NatsCon,
+  broker: NatsCon,
+  orders: HashMap<ObjectId, Order>,
+  positions: HashMap<ObjectId, OrderInner>,
 }
+
+impl Executor {
+  pub fn new(broker: NatsCon) -> Self {
+    return Self {
+      broker,
+      orders: HashMap::new(),
+      positions: HashMap::new(),
+    };
+  }
+}
+
+// impl ExecutorTrait for Executor {
+//   async fn create_order(
+//     &mut self,
+//     symbol: String,
+//     price: Option<f64>,
+//     budget: f64,
+//     order_option: Option<OrderOption>,
+//   ) -> GenericResult<ObjectId> {
+//   }
+
+//   async fn remove_order(
+//     &mut self,
+//     id: ObjectId,
+//   ) -> GenericResult<ExecutionResult> {
+//   }
+// }
