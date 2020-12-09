84fca72c39fecbb98ad99878bb693d8021c24418
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-12-09 13:27:21 +0900

    Seems it needs test-trait

--- backend/libs/exchanges/src/binance/executors/front_test.rs
+++ backend/libs/exchanges/src/binance/executors/front_test.rs
@@ -1,10 +1,17 @@
 use ::std::collections::HashMap;
 
+use ::async_stream::stream;
+use ::async_trait::async_trait;
+use ::futures::stream::{LocalBoxStream, StreamExt};
 use ::mongodb::bson::oid::ObjectId;
 use ::nats::asynk::Connection as NatsCon;
 use ::slog::Logger;
 
-use crate::traits::Executor as ExecutorTrait;
+use ::types::{ret_on_err, GenericResult};
+
+use crate::traits::{
+  Executor as ExecutorTrait, TradeObserver as TradeObserverTrait,
+};
 
 use crate::entities::BookTicker;
 
@@ -35,5 +42,39 @@ impl Executor {
       maker_fee,
       taker_fee,
     };
+  }
+}
+
+#[async_trait]
+impl ExecutorTrait for Executor {
+  async fn open(
+    &mut self,
+  ) -> GenericResult<LocalBoxStream<'_, GenericResult<BookTicker>>> {
+    let observer = self.observer.clone();
+    let mut src_stream =
+      ret_on_err!(observer.subscribe().await).map(|ticker| return ticker);
+    let stream = stream! {
+      while let Some(v) = src_stream.next().await {
+        self.cur_trade = Some(v.clone());
+        yield GenericResult::<BookTicker>::Ok(v);
+      }
+      self.cur_trade = None;
+    };
+    return Ok(Box::pin(stream.boxed_local());
+  }
+
+  async fn create_order(
+    &mut self,
+    symbol: String,
+    price: Option<f64>,
+    budget: f64,
+    order_option: Option<OrderOption>,
+  ) -> GenericResult<ObjectId> {
+  }
+
+  async fn remove_order(
+    &mut self,
+    id: ObjectId,
+  ) -> GenericResult<ExecutionResult> {
   }
 }
