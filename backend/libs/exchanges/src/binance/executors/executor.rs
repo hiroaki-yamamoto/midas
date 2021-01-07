1472dfa46cf5e9e374734984ec5f1d62fcc8e480
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-01-07 11:01:40 +0900

    Added Executor.new method

--- backend/libs/exchanges/src/binance/executors/executor.rs
+++ backend/libs/exchanges/src/binance/executors/executor.rs
@@ -1,5 +1,19 @@
-use super::super::observer::TradeObserver;
+use ::mongodb::{Database, Collection};
+
+use crate::KeyChain;
 
 pub struct Executor {
-  observer: TradeObserver,
+  keychain: KeyChain,
+  db: Database,
+  orders: Collection,
+  positions: Collection,
+}
+
+impl Executor {
+  pub async fn new(db: Database) -> Self {
+    let keychain = KeyChain::new(db.clone()).await;
+    let orders = db.collection("binance.orders");
+    let positions = db.collection("binance.positions");
+    return Self{ keychain, db, orders, positions };
+  }
 }
