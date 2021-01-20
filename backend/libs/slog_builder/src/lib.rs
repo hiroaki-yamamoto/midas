37770ff86f9368ea9506985a047efeea81a56b8a
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-01-20 09:11:25 +0900

    Removed old code

--- backend/libs/slog_builder/src/lib.rs
+++ backend/libs/slog_builder/src/lib.rs
@@ -7,15 +7,12 @@ fn new_root_logger<T>(drain: T) -> Logger
 where
   T: SendSyncRefUnwindSafeDrain<Ok = (), Err = Never> + UnwindSafe + 'static,
 {
-  // let drain = ::slog_async::Async::new(drain).build().fuse();
-  // let drain = AtomicSwitch::new(drain);
   return Logger::root(drain, o!("version" => env!("CARGO_PKG_VERSION")));
 }
 
 pub fn build_debug() -> Logger {
   let dec = ::slog_term::TermDecorator::new().build();
   let drain = ::slog_term::FullFormat::new(dec).build().fuse();
-  // let drain = Async::new(drain).chan_size(8192).build().fuse();
   return new_root_logger(Mutex::new(drain).fuse());
 }
 
@@ -24,6 +21,5 @@ pub fn build_json() -> Logger {
     .add_default_keys()
     .build()
     .fuse();
-  // let drain = Async::new(drain).chan_size(8192).build().fuse();s
   return new_root_logger(Mutex::new(drain).fuse());
 }
