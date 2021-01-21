163cbfee25449009b69dc53bafc9784f3f4ce2c2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-01-21 16:00:59 +0900

    Changed mongodb to use async-std

--- backend/libs/rpc/src/historical.rs
+++ backend/libs/rpc/src/historical.rs
@@ -2,29 +2,29 @@
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct HistChartProg {
-  #[prost(string, tag = "1")]
-  pub symbol: ::prost::alloc::string::String,
-  #[prost(int64, tag = "2")]
-  pub num_symbols: i64,
-  #[prost(int64, tag = "3")]
-  pub cur_symbol_num: i64,
-  #[prost(int64, tag = "4")]
-  pub num_objects: i64,
-  #[prost(int64, tag = "5")]
-  pub cur_object_num: i64,
+    #[prost(string, tag="1")]
+    pub symbol: ::prost::alloc::string::String,
+    #[prost(int64, tag="2")]
+    pub num_symbols: i64,
+    #[prost(int64, tag="3")]
+    pub cur_symbol_num: i64,
+    #[prost(int64, tag="4")]
+    pub num_objects: i64,
+    #[prost(int64, tag="5")]
+    pub cur_object_num: i64,
 }
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct HistChartFetchReq {
-  #[prost(string, repeated, tag = "2")]
-  #[serde(rename = "symbolsList")]
-  pub symbols: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[prost(string, repeated, tag="2")]
+    #[serde(rename = "symbolsList")]
+    pub symbols: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct StopRequest {
-  #[prost(enumeration = "super::entities::Exchanges", repeated, tag = "1")]
-  pub exchanges: ::prost::alloc::vec::Vec<i32>,
+    #[prost(enumeration="super::entities::Exchanges", repeated, tag="1")]
+    pub exchanges: ::prost::alloc::vec::Vec<i32>,
 }
--- backend/libs/rpc/src/keychain.rs
+++ backend/libs/rpc/src/keychain.rs
@@ -2,29 +2,29 @@
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ApiKey {
-  #[prost(string, tag = "1")]
-  pub id: ::prost::alloc::string::String,
-  #[prost(enumeration = "super::entities::Exchanges", tag = "2")]
-  pub exchange: i32,
-  #[prost(string, tag = "3")]
-  pub label: ::prost::alloc::string::String,
-  #[prost(string, tag = "4")]
-  pub pub_key: ::prost::alloc::string::String,
-  #[prost(string, tag = "5")]
-  pub prv_key: ::prost::alloc::string::String,
+    #[prost(string, tag="1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(enumeration="super::entities::Exchanges", tag="2")]
+    pub exchange: i32,
+    #[prost(string, tag="3")]
+    pub label: ::prost::alloc::string::String,
+    #[prost(string, tag="4")]
+    pub pub_key: ::prost::alloc::string::String,
+    #[prost(string, tag="5")]
+    pub prv_key: ::prost::alloc::string::String,
 }
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ApiKeyList {
-  #[prost(message, repeated, tag = "1")]
-  #[serde(rename = "keysList")]
-  pub keys: ::prost::alloc::vec::Vec<ApiKey>,
+    #[prost(message, repeated, tag="1")]
+    #[serde(rename = "keysList")]
+    pub keys: ::prost::alloc::vec::Vec<ApiKey>,
 }
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ApiRename {
-  #[prost(string, tag = "1")]
-  pub label: ::prost::alloc::string::String,
+    #[prost(string, tag="1")]
+    pub label: ::prost::alloc::string::String,
 }
