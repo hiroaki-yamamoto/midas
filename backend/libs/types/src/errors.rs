de280ff49854efc249d57b9bcb829fc5fdff530d
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-03-27 19:29:32 +0900

    Use error crate's ParseError for casting

--- backend/libs/types/src/errors.rs
+++ backend/libs/types/src/errors.rs
@@ -3,31 +3,6 @@ use ::std::fmt::{Display, Formatter, Result as FormatResult};
 
 use ::serde::Serialize;
 
-#[derive(Debug, Clone)]
-pub struct ParseError {
-  fld_name: String,
-}
-
-impl ParseError {
-  pub fn new(fld_name: &str) -> Self {
-    return ParseError {
-      fld_name: fld_name.into(),
-    };
-  }
-}
-
-impl Display for ParseError {
-  fn fmt(&self, f: &mut Formatter<'_>) -> FormatResult {
-    return write!(f, "Failed to parse '{}", self.fld_name);
-  }
-}
-
-impl Error for ParseError {
-  fn source(&self) -> Option<&(dyn Error + 'static)> {
-    None
-  }
-}
-
 #[derive(Debug, Clone, Serialize)]
 pub struct VecElementErr<T>
 where
