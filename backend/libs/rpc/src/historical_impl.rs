90510f52ebc2f9bbdd03b13d1c7dcdd0016cedd5
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-04-05 11:01:41 +0900

    Implemented arithmetic add/sub operations to HistChartProg

---
+++ backend/libs/rpc/src/historical_impl.rs
@@ -0,0 +1,44 @@
+use ::std::ops::{Add, Sub};
+
+use ::http::status::StatusCode;
+
+use super::entities::Status;
+use super::historical::HistChartProg;
+
+impl Sub for HistChartProg {
+  type Output = Result<HistChartProg, Status>;
+  fn sub(self, rhs: Self) -> Self::Output {
+    if self.symbol != rhs.symbol {
+      return Err(Status::new(
+        StatusCode::EXPECTATION_FAILED,
+        "The symbol must be the same for each other.",
+      ));
+    }
+    return Ok(HistChartProg {
+      symbol: self.symbol,
+      num_symbols: self.num_symbols - rhs.num_symbols,
+      num_objects: self.num_objects - rhs.num_objects,
+      cur_symbol_num: self.cur_symbol_num - rhs.cur_symbol_num,
+      cur_object_num: self.cur_object_num - rhs.cur_object_num,
+    });
+  }
+}
+
+impl Add for HistChartProg {
+  type Output = Result<HistChartProg, Status>;
+  fn add(self, rhs: Self) -> Self::Output {
+    if self.symbol != rhs.symbol {
+      return Err(Status::new(
+        StatusCode::EXPECTATION_FAILED,
+        "The symbol must be the same for each other.",
+      ));
+    }
+    return Ok(HistChartProg {
+      symbol: self.symbol,
+      num_symbols: self.num_symbols + rhs.num_symbols,
+      num_objects: self.num_objects + rhs.num_objects,
+      cur_symbol_num: self.cur_symbol_num + rhs.cur_symbol_num,
+      cur_object_num: self.cur_object_num + rhs.cur_object_num,
+    });
+  }
+}
--- backend/workers/historical/src/main_aggregator.rs
+++ backend/workers/historical/src/main_aggregator.rs
@@ -6,20 +6,34 @@ use ::libc::{SIGINT, SIGTERM};
 use ::nats::connect;
 use ::rpc::entities::Exchanges;
 use ::rpc::historical::HistChartProg;
-use ::slog::{info, o};
+use ::tokio::select;
 use ::tokio::signal::unix as signal;
 
 use ::binance_histories::pubsub::HistProgPartPubSub;
 use ::config::{CmdArgs, Config};
-use ::history::pubsub::FetchStatusPubSub;
+use ::history::FetchStatusPubSub;
+use ::subscribe::PubSub;
 
 #[tokio::main]
 async fn main() {
   let args: CmdArgs = CmdArgs::parse();
   let cfg = Config::from_fpath(Some(args.config)).unwrap();
   let logger = cfg.build_slog();
-  info!(logger, "Kline fetch worker");
+  ::slog::info!(logger, "Kline fetch worker");
   let broker = connect(&cfg.broker_url).unwrap();
 
   let kvs: HashMap<Exchanges, HashMap<String, HistChartProg>> = HashMap::new();
+  let part = HistProgPartPubSub::new(broker.clone());
+  let (part_handler, part) = part.queue_subscribe("aggregate").unwrap();
+  let result_st = FetchStatusPubSub::new(broker);
+  let mut stop =
+    signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT)).unwrap();
+  loop {
+    select! {
+      _ = stop.recv() => {
+        let _ = part_handler.unsubscribe().unwrap();
+        break;
+      },
+    };
+  }
 }
