19f0d5e3752d83ae36559e875ba06badd2d7e1d4
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-05-27 09:59:19 +0900

    Include bot rpc entity module to rpc crate
    To be honest, I don't think the use of include!("./google.protobuf.rs") is good idea,
    but there's no other way...

--- backend/libs/rpc/src/lib.rs
+++ backend/libs/rpc/src/lib.rs
@@ -1,7 +1,14 @@
 pub mod bookticker;
+pub mod bot;
 pub mod entities;
 mod entities_impl;
 pub mod historical;
 mod historical_impl;
 pub mod keychain;
 pub mod rejection_handler;
+
+pub mod google {
+  pub mod protobuf {
+    include!("./google.protobuf.rs");
+  }
+}
