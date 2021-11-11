ebf13c4c87edab97b9a27221b567d043126dd5c2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-11-11 13:33:05 +0900

    Introduced low-level dependency libraries build scripts

--- backend/build.sh
+++ backend/build.sh
@@ -3,11 +3,13 @@
 
 set -e
 
+DEPS=`realpath ../deps`
+
 export \
   PKG_CONFIG_ALLOW_CROSS=1 \
   OPENSSL_STATIC=true \
-  OPENSSL_DIR=${HOME}/opt/openssl-musl/musl \
-  OPENSSL_LIB_DIR=${HOME}/opt/openssl-musl/musl/lib64 \
+  OPENSSL_DIR=$DEPS/openssl \
+  OPENSSL_LIB_DIR=$DEPS/openssl/lib64 \
   CARGO_INCREMENTAL=false
 
 cargo build --target x86_64-unknown-linux-musl $@
