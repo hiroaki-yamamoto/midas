7b2b8416f7f3f428cc08945a7958f87d7b1472e4
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2021-12-18 17:23:07 +0900

    Optimized build config

--- backend/build.sh
+++ backend/build.sh
@@ -2,14 +2,4 @@
 # -*- coding: utf-8 -*-
 
 set -e
-
-DEPS=`realpath ../deps`
-
-export \
-  PKG_CONFIG_ALLOW_CROSS=1 \
-  OPENSSL_STATIC=true \
-  OPENSSL_DIR=$DEPS/openssl \
-  OPENSSL_LIB_DIR=$DEPS/openssl/lib64 \
-  CARGO_INCREMENTAL=false
-
 cargo build --target x86_64-unknown-linux-musl $@
