2eaafd095b514e2fa95f27bbd83fbfc752afe104
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2022-06-09 20:27:06 +0900

    Revert "Use iter"
    This reverts commit 5e5429577344ae720ceb50bbc1e7f9e72dcf907a.

--- backend/libs/subscribe/src/streams.rs
+++ backend/libs/subscribe/src/streams.rs
@@ -1,12 +1,23 @@
-use ::futures::{stream::iter, Stream, StreamExt};
+use ::std::io::{Error as IOError, ErrorKind};
+
+use ::futures::{Stream, StreamExt};
 use ::nats::{Message, Subscription};
 use ::rmp_serde::from_slice as from_msgpack;
 use ::serde::de::DeserializeOwned;
+use ::tokio::sync::mpsc::unbounded_channel;
+use ::tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub fn to_stream_raw(
   sub: Subscription,
 ) -> impl Stream<Item = Message> + Send + Sync {
-  return iter(sub);
+  let (sender, receiver) = unbounded_channel();
+  let _ = sub.with_handler(move |msg| {
+    return sender
+      .send(msg)
+      .map_err(|e| IOError::new(ErrorKind::Other, e));
+  });
+  let stream = UnboundedReceiverStream::new(receiver);
+  return stream;
 }
 
 pub fn to_stream<T>(
