5e5429577344ae720ceb50bbc1e7f9e72dcf907a
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2022-06-09 20:24:31 +0900

    Use iter

--- backend/libs/subscribe/src/streams.rs
+++ backend/libs/subscribe/src/streams.rs
@@ -1,23 +1,12 @@
-use ::std::io::{Error as IOError, ErrorKind};
-
-use ::futures::{Stream, StreamExt};
+use ::futures::{stream::iter, Stream, StreamExt};
 use ::nats::{Message, Subscription};
 use ::rmp_serde::from_slice as from_msgpack;
 use ::serde::de::DeserializeOwned;
-use ::tokio::sync::mpsc::unbounded_channel;
-use ::tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub fn to_stream_raw(
   sub: Subscription,
 ) -> impl Stream<Item = Message> + Send + Sync {
-  let (sender, receiver) = unbounded_channel();
-  let _ = sub.with_handler(move |msg| {
-    return sender
-      .send(msg)
-      .map_err(|e| IOError::new(ErrorKind::Other, e));
-  });
-  let stream = UnboundedReceiverStream::new(receiver);
-  return stream;
+  return iter(sub);
 }
 
 pub fn to_stream<T>(
