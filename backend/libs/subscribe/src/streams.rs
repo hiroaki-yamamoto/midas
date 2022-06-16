229dd4fd8cdb573c2942ec79b955dc62a1759263
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2022-06-16 17:15:16 +0900

    Added a stream named 'Sub' that can return Pending status
    but the event loop doesn't work yet...:poop:

--- backend/libs/subscribe/src/streams.rs
+++ backend/libs/subscribe/src/streams.rs
@@ -1,24 +1,36 @@
-use ::std::io::{Error as IOError, ErrorKind};
-
+use ::futures::task::Poll;
 use ::futures::{Stream, StreamExt};
 use ::nats::{Message, Subscription as NatsSub};
 use ::rmp_serde::from_slice as from_msgpack;
 use ::serde::de::DeserializeOwned;
 
-use ::tokio::sync::mpsc::unbounded_channel;
-use ::tokio_stream::wrappers::UnboundedReceiverStream;
+pub struct Sub {
+  sub: NatsSub,
+}
+
+impl Sub {
+  pub fn new(sub: NatsSub) -> Self {
+    return Self { sub };
+  }
+}
+
+impl Stream for Sub {
+  type Item = Message;
+  fn poll_next(
+    self: std::pin::Pin<&mut Self>,
+    _: &mut std::task::Context<'_>,
+  ) -> Poll<Option<Self::Item>> {
+    return match self.sub.try_next() {
+      Some(v) => Poll::Ready(Some(v)),
+      None => Poll::Pending,
+    };
+  }
+}
 
 pub fn to_stream_raw(
   sub: NatsSub,
 ) -> impl Stream<Item = Message> + Send + Sync {
-  let (sender, receiver) = unbounded_channel();
-  let _ = sub.with_handler(move |msg| {
-    return sender
-      .send(msg)
-      .map_err(|e| IOError::new(ErrorKind::Other, e));
-  });
-  let stream = UnboundedReceiverStream::new(receiver);
-  return stream;
+  return Sub::new(sub);
 }
 
 pub fn to_stream<T>(
