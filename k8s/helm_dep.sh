36201741a452c2e10935495a7d6784e8cdd57d77
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2022-06-20 15:13:01 +0900

    Use helm to install nats

---
+++ k8s/helm_dep.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+set -e
+
+WORKDIR=`dirname $0`
+
+if [ ! -x `which helm` ]; then
+  echo "Install Helm"
+  exit 1
+fi
+
+if [ ! -x `which yq` ]; then
+  echo "Install yq: https://github.com/mikefarah/yq"
+  exit 1
+fi
+
+addRepo() {
+  local name=$1
+  local repo=$2
+  echo "Adding '$2' as '$1'"
+  helm repo add $1 $2
+}
+
+helmInstall() {
+  echo "Installing $2 as $1"
+  if [ -n "$3" ]; then
+    echo "Helm Exra ARGS: ${@:3}"
+  fi
+  helm upgrade -i $@
+}
+
+NAMESPACE_PATH="${WORKDIR}/./namespace.yml"
+MIDAS_NAMESPACE=`yq '.metadata.name' $NAMESPACE_PATH`
+
+echo "Refreshing the release menu in the repo"
+helm repo update
+
+addRepo 'nats' 'https://nats-io.github.io/k8s/helm/charts/'
+helmInstall broker nats/nats \
+  -n $MIDAS_NAMESPACE --create-namespace \
+  -f $WORKDIR/./broker/config.yml
+kubectl apply -f $WORKDIR/./broker/svc.yml -n midas
+BROKER_HOST=`yq '.metadata.name' $WORKDIR/./broker/svc.yml`
+helmInstall stan nats/stan \
+  -n $MIDAS_NAMESPACE --create-namespace \
+  --set stan.nats.url="nats://$BROKER_HOST.$MIDAS_NAMESPACE.svc.cluster.local:4222" \
+  --set stan.replicas=1 \
+  --set store.type="memory"
