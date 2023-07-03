9f94bdfa4d4b4fb675e5b79d4d93d11f2233a8e0
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-07-03 09:15:22 +0900

    Added init function that initializes essential stuff

--- backend/libs/config/src/lib.rs
+++ backend/libs/config/src/lib.rs
@@ -2,6 +2,12 @@ mod cmdargs;
 mod config;
 mod constants;
 
+use std::future::Future;
+use std::net::SocketAddr;
+
+use ::clap::Parser;
+use ::libc::{SIGINT, SIGTERM};
+
 pub use self::constants::{
   CHAN_BUF_SIZE, DEFAULT_CONFIG_PATH, DEFAULT_RECONNECT_INTERVAL,
   NUM_OBJECTS_TO_FETCH,
@@ -9,4 +15,23 @@ pub use self::constants::{
 
 pub use self::cmdargs::CmdArgs;
 pub use self::config::Config;
-pub use ::redis;
+
+use ::mongodb::Database;
+use ::nats::jetstream::JetStream as Broker;
+use ::tokio::signal::unix as signal;
+
+pub async fn init<S, T>(func: T)
+where
+  T: Fn(Config, signal::Signal, Database, Broker, SocketAddr) -> S,
+  S: Future<Output = ()>,
+{
+  let sig =
+    signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT)).unwrap();
+  let args: CmdArgs = CmdArgs::parse();
+  let cfg = Config::from_fpath(Some(args.config)).unwrap();
+  cfg.init_logger();
+  let db = cfg.db().await.unwrap();
+  let broker = cfg.nats_cli().unwrap();
+  let host: SocketAddr = cfg.host.parse().unwrap();
+  func(cfg, sig, db, broker, host).await;
+}
