6690bf6c862ce2215cdc9e02dddf5a3c1b01228b
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-07-03 10:23:16 +0900

    [historical] use init

--- backend/services/historical/src/main.rs
+++ backend/services/historical/src/main.rs
@@ -1,15 +1,10 @@
 mod service;
 
-use ::std::net::SocketAddr;
-
-use ::clap::Parser;
 use ::futures::FutureExt;
-use ::libc::{SIGINT, SIGTERM};
 use ::log::{info, warn};
-use ::tokio::signal::unix as signal;
 use ::warp::Filter;
 
-use ::config::{CmdArgs, Config};
+use ::config::init;
 use ::csrf::{CSRFOption, CSRF};
 use ::probe::probe;
 use ::rpc::rejection_handler::handle_rejection;
@@ -18,36 +13,31 @@ use crate::service::Service;
 
 #[tokio::main]
 async fn main() {
-  let args: CmdArgs = CmdArgs::parse();
-  let cfg = Config::from_fpath(Some(args.config)).unwrap();
-  cfg.init_logger();
-  info!("Historical Kline Service");
-  let broker = cfg.nats_cli().unwrap();
-  let redis = cfg.redis;
-  let host: SocketAddr = cfg.host.parse().unwrap();
-  let svc = Service::new(&broker, &redis).await;
-  let csrf = CSRF::new(CSRFOption::builder());
-  let access_log = ::access_logger::log();
-  let route = csrf
-    .protect()
-    .and(svc.route())
-    .or(probe())
-    .recover(handle_rejection)
-    .with(access_log);
+  init(|cfg, mut sig, _, broker, host| async move {
+    info!("Historical Kline Service");
+    let svc = Service::new(&broker, &cfg.redis).await;
+    let csrf = CSRF::new(CSRFOption::builder());
+    let access_log = ::access_logger::log();
+    let route = csrf
+      .protect()
+      .and(svc.route())
+      .or(probe())
+      .recover(handle_rejection)
+      .with(access_log);
 
-  let mut sig =
-    signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT)).unwrap();
-  let host = host.clone();
-  info!("Opened REST server on {}", host);
-  let (_, ws_svr) = ::warp::serve(route)
-    .tls()
-    .cert_path(&cfg.tls.cert)
-    .key_path(&cfg.tls.prv_key)
-    .bind_with_graceful_shutdown(host, async move {
-      sig.recv().await;
+    let host = host.clone();
+    info!("Opened REST server on {}", host);
+    let (_, ws_svr) = ::warp::serve(route)
+      .tls()
+      .cert_path(&cfg.tls.cert)
+      .key_path(&cfg.tls.prv_key)
+      .bind_with_graceful_shutdown(host, async move {
+        sig.recv().await;
+      });
+    let svr = ws_svr.then(|_| async {
+      warn!("REST Server is shutting down! Bye! Bye!");
     });
-  let svr = ws_svr.then(|_| async {
-    warn!("REST Server is shutting down! Bye! Bye!");
-  });
-  svr.await;
+    svr.await;
+  })
+  .await;
 }
