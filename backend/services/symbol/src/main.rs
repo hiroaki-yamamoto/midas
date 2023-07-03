493ec5e454930a5e4d83f327d1d6ca722ccb43a8
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-07-03 09:50:16 +0900

    Use init

--- backend/services/symbol/src/main.rs
+++ backend/services/symbol/src/main.rs
@@ -1,16 +1,11 @@
 mod service;
 
-use ::std::net::SocketAddr;
-
-use ::clap::Parser;
 use ::futures::FutureExt;
-use ::libc::{SIGINT, SIGTERM};
 use ::log::{info, warn};
-use ::tokio::signal::unix as signal;
 use ::warp::Filter;
 
 use ::access_logger::log;
-use ::config::{CmdArgs, Config};
+use ::config::init;
 use ::csrf::{CSRFOption, CSRF};
 use ::probe::probe;
 use ::rpc::rejection_handler::handle_rejection;
@@ -19,33 +14,27 @@ use self::service::Service;
 
 #[tokio::main]
 async fn main() {
-  let mut sig =
-    signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT)).unwrap();
-  let args: CmdArgs = CmdArgs::parse();
-  let cfg = Config::from_fpath(Some(args.config)).unwrap();
-  cfg.init_logger();
-  let db = cfg.db().await.unwrap();
-  let broker = cfg.nats_cli().unwrap();
-  let host: SocketAddr = cfg.host.parse().unwrap();
-  let svc = Service::new(&db, &broker).await.unwrap();
-  let csrf = CSRF::new(CSRFOption::builder());
-  let router = csrf
-    .protect()
-    .and(probe().or(svc.route()))
-    .with(log())
-    .recover(handle_rejection);
+  init(|cfg, mut sig, db, broker, host| async move {
+    let svc = Service::new(&db, &broker).await.unwrap();
+    let csrf = CSRF::new(CSRFOption::builder());
+    let router = csrf
+      .protect()
+      .and(probe().or(svc.route()))
+      .with(log())
+      .recover(handle_rejection);
 
-  info!("Opened REST server on {}", host);
-  let (_, svr) = ::warp::serve(router)
-    .tls()
-    .cert_path(&cfg.tls.cert)
-    .key_path(&cfg.tls.prv_key)
-    .bind_with_graceful_shutdown(host, async move {
-      sig.recv().await;
+    info!("Opened REST server on {}", host);
+    let (_, svr) = ::warp::serve(router)
+      .tls()
+      .cert_path(&cfg.tls.cert)
+      .key_path(&cfg.tls.prv_key)
+      .bind_with_graceful_shutdown(host, async move {
+        sig.recv().await;
+      });
+    let svr = svr.then(|_| async {
+      warn!("REST Server is shutting down! Bye! Bye!");
     });
-  let svr = svr.then(|_| async {
-    warn!("REST Server is shutting down! Bye! Bye!");
-  });
-  svr.await;
-  return;
+    svr.await;
+  })
+  .await;
 }
