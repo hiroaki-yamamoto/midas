7b6c8e45b90ba72979c1768285bde42a22b7e27e
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-07-03 09:18:18 +0900

    [bot] use init

--- backend/services/bot/src/main.rs
+++ backend/services/bot/src/main.rs
@@ -1,16 +1,11 @@
 mod routing;
 
-use ::std::net::SocketAddr;
-
-use ::clap::Parser;
 use ::futures::FutureExt;
-use ::libc::{SIGINT, SIGTERM};
 use ::log::{info, warn};
-use ::tokio::signal::unix as signal;
 use ::warp::Filter;
 
 use ::access_logger::log;
-use ::config::{CmdArgs, Config};
+use ::config::init;
 use ::csrf::{CSRFOption, CSRF};
 use ::rpc::rejection_handler::handle_rejection;
 
@@ -18,33 +13,28 @@ use self::routing::construct;
 
 #[tokio::main]
 async fn main() {
-  let mut sig =
-    signal::signal(signal::SignalKind::from_raw(SIGTERM | SIGINT)).unwrap();
-  let args: CmdArgs = CmdArgs::parse();
-  let cfg = Config::from_fpath(Some(args.config)).unwrap();
-  cfg.init_logger();
-  let access_logger = log();
-  let db = cfg.db().await.unwrap();
-  let http_cli = cfg.build_rest_client().unwrap();
-  let host: SocketAddr = cfg.host.parse().unwrap();
-  let csrf = CSRF::new(CSRFOption::builder());
-  let route = construct(&db, http_cli, &cfg.transpiler_url);
-  let route = csrf
-    .protect()
-    .and(route)
-    .with(access_logger)
-    .recover(handle_rejection);
-
-  info!("Opened REST server on {}", host);
-  let (_, svr) = ::warp::serve(route)
-    .tls()
-    .cert_path(&cfg.tls.cert)
-    .key_path(&cfg.tls.prv_key)
-    .bind_with_graceful_shutdown(host, async move {
-      sig.recv().await;
+  init(|cfg, mut sig, db, _, host| async move {
+    let access_logger = log();
+    let http_cli = cfg.build_rest_client().unwrap();
+    let csrf = CSRF::new(CSRFOption::builder());
+    let route = construct(&db, http_cli, &cfg.transpiler_url);
+    let route = csrf
+      .protect()
+      .and(route)
+      .with(access_logger)
+      .recover(handle_rejection);
+    info!("Opened REST server on {}", host);
+    let (_, svr) = ::warp::serve(route)
+      .tls()
+      .cert_path(&cfg.tls.cert)
+      .key_path(&cfg.tls.prv_key)
+      .bind_with_graceful_shutdown(host, async move {
+        sig.recv().await;
+      });
+    let svr = svr.then(|_| async {
+      warn!("REST Server is shutting down! Bye! Bye!");
     });
-  let svr = svr.then(|_| async {
-    warn!("REST Server is shutting down! Bye! Bye!");
-  });
-  svr.await;
+    svr.await;
+  })
+  .await;
 }
