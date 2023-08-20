acf19b5ddafeb0a9f3f88eea4608576a70e6dae6
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-08-20 15:41:47 +0900

    pub use error

--- backend/libs/subscribe/src/lib.rs
+++ backend/libs/subscribe/src/lib.rs
@@ -6,6 +6,7 @@ pub use ::tokio::time::sleep;
 pub use self::traits::PubSub;
 pub use ::async_nats as nats;
 pub use ::async_nats::jetstream as natsJS;
+pub use ::errors;
 
 #[macro_export]
 macro_rules! pubsub {
@@ -24,7 +25,7 @@ macro_rules! pubsub {
     impl $name {
       async fn add_stream(
         ctx: &::subscribe::natsJS::context::Context,
-      ) -> ::errors::CreateStreamResult<::subscribe::natsJS::stream::Stream> {
+      ) -> ::subscribe::errors::CreateStreamResult<::subscribe::natsJS::stream::Stream> {
         let mut option: ::subscribe::natsJS::stream::Config = $id.into();
         option.max_consumers = -1;
         return ctx.get_or_create_stream(option).await;
@@ -32,7 +33,7 @@ macro_rules! pubsub {
 
       pub async fn new(
         ctx: &::subscribe::natsJS::context::Context,
-      ) -> ::errors::CreateStreamResult<Self> {
+      ) -> ::subscribe::errors::CreateStreamResult<Self> {
         let stream = Self::add_stream(ctx).await?;
         let mut me = Self { stream, ctx: ctx.clone() };
         return Ok(me);
