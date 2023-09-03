b06f7e98fa65b9d28c36b9d9e77a9eb6e946d04b
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-09-03 12:19:38 +0900

    Added get_nodes_by_exchange to SoftExpirationStore

--- backend/libs/exchanges/observers/src/kvs.rs
+++ backend/libs/exchanges/observers/src/kvs.rs
@@ -3,6 +3,7 @@ use ::futures::stream::{iter, BoxStream, StreamExt};
 use ::kvs::kvs;
 use ::kvs::redis::{Commands, RedisResult};
 use ::kvs::{SoftExpirationStore, Store};
+use ::rpc::entities::Exchanges;
 
 kvs!(pub, ObserverNodeKVS, String, "observer_node:{}");
 kvs!(pub, ONEXTypeKVS, String, "observer_node_exchange_type:{}");
@@ -23,6 +24,44 @@ impl<T> SoftExpirationStore<T, String> for ObserverNodeKVS<T> where
   T: Commands + Send + Sync
 {
 }
+
+impl<T> ONEXTypeKVS<T>
+where
+  T: Commands + Send + Sync,
+{
+  pub fn get_nodes_by_exchange(
+    &self,
+    exchange: Exchanges,
+  ) -> RedisResult<BoxStream<'_, String>> {
+    let keys: Vec<String> = self
+      .lock_commands()
+      .scan_match("observer_node_exchange_type:*")?
+      .collect();
+    let keys = keys
+      .into_iter()
+      .map(|key| {
+        let mut cmd = self.lock_commands();
+        return (key.clone(), cmd.get::<_, String>(key));
+      })
+      .filter_map(|(key, exchange)| {
+        let pair =
+          exchange.map(|exchange| (key, Exchanges::from_str_name(&exchange)));
+        return pair.ok();
+      })
+      .filter_map(|(key, node_exchange)| {
+        return node_exchange.map(|node_exchange| (key, node_exchange));
+      })
+      .filter_map(move |(key, node_exchange)| {
+        if node_exchange == exchange {
+          return Some(key);
+        } else {
+          return None;
+        }
+      });
+    return Ok(iter(keys).boxed());
+  }
+}
+
 impl<T> SoftExpirationStore<T, String> for ONEXTypeKVS<T> where
   T: Commands + Send + Sync
 {
