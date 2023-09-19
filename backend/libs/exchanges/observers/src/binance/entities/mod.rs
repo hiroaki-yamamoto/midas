5f62af184fbad87124adda1f51f2eb4bc78855c6
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-09-19 12:48:04 +0900

    chore(trade_observer): Added ResultValue that is returned by binance websocket server

--- backend/libs/exchanges/observers/src/binance/entities/mod.rs
+++ backend/libs/exchanges/observers/src/binance/entities/mod.rs
@@ -1,6 +1,8 @@
 mod book_ticker;
+mod result;
 mod subscribe;
 
 pub use self::subscribe::{SubscribeRequest, SubscribeRequestInner};
 
 pub use self::book_ticker::BookTicker;
+pub use self::result::ResultValue;
