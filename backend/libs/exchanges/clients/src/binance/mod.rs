0cd957f950286949b8c5cf1fee9db3694f770e6e
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-09-19 17:36:00 +0900

    chore: :construction: Websocket should be round lib

--- backend/libs/exchanges/clients/src/binance/mod.rs
+++ backend/libs/exchanges/clients/src/binance/mod.rs
@@ -1,7 +1,5 @@
 mod api_header;
 mod constants;
-mod ws;
 
 pub use self::api_header::{APIHeader, FindKey};
 pub use self::constants::{REST_ENDPOINTS, WS_ENDPOINT};
-pub use self::ws::WebSocket;
--- backend/libs/round/src/ws.rs
+++ backend/libs/round/src/ws.rs
@@ -5,20 +5,18 @@ use ::tokio_tungstenite::connect_async;
 use ::errors::{StatusFailure, WebSocketInitResult};
 use ::types::TLSWebSocket;
 
-use super::WS_ENDPOINT;
-
 pub struct WebSocket {
   socket: TLSWebSocket,
 }
 
 impl WebSocket {
-  fn choose_url() -> String {
+  fn choose_url(endpoints: &[&str]) -> String {
     let mut rng = thread_rng();
-    let index: usize = rng.gen_range(0..WS_ENDPOINT.len());
-    return WS_ENDPOINT[index].to_string();
+    let index: usize = rng.gen_range(0..endpoints.len());
+    return endpoints[index].to_string();
   }
-  pub async fn new() -> WebSocketInitResult<Self> {
-    let url = Self::choose_url();
+  pub async fn new(endpoints: &[&str]) -> WebSocketInitResult<Self> {
+    let url = Self::choose_url(endpoints);
     let (socket, resp) = connect_async(&url).await?;
     if resp.status().is_client_error() || resp.status().is_server_error() {
       let err = StatusFailure::new(
