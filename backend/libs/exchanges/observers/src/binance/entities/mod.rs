349880a558808fbe11ca19d03dcffefd0a71d1ea
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-09-22 15:14:57 +0900

    Implemented untagged WebsocketPayload enum

--- backend/libs/exchanges/observers/src/binance/entities/mod.rs
+++ backend/libs/exchanges/observers/src/binance/entities/mod.rs
@@ -2,7 +2,16 @@ mod book_ticker;
 mod result;
 mod subscribe;
 
+use ::serde::Deserialize;
+
 pub use self::subscribe::{SubscribeRequest, SubscribeRequestInner};
 
 pub use self::book_ticker::BookTicker;
 pub use self::result::ResultValue;
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub enum WebsocketPayload {
+  BookTicker(BookTicker<String>),
+  Result(ResultValue),
+}
