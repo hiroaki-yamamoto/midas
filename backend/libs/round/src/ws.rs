fa7252004adf87e702145e93c39e08186b745836
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-10-08 12:22:59 +0900

    fix(ws): Use async for ws

--- backend/libs/round/src/ws.rs
+++ backend/libs/round/src/ws.rs
@@ -1,5 +1,6 @@
 use ::std::marker::PhantomData;
 use ::std::pin::Pin;
+use ::std::sync::Arc;
 use ::std::task::{Context, Poll};
 use ::std::time::Duration;
 
@@ -12,6 +13,7 @@ use ::rand::Rng;
 use ::serde::{de::DeserializeOwned, ser::Serialize};
 use ::serde_json::{from_str as json_parse, to_string as jsonify};
 use ::tokio::runtime::Runtime;
+use ::tokio::sync::RwLock;
 use ::tokio::time::interval;
 use ::tokio_tungstenite::connect_async;
 use ::tokio_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
@@ -32,9 +34,9 @@ where
   R: DeserializeOwned + Unpin,
   W: Serialize + Unpin,
 {
-  socket: TLSWebSocket,
+  socket: Arc<RwLock<TLSWebSocket>>,
   endpoints: Vec<String>,
-  runtime: Runtime,
+  runtime: Arc<Runtime>,
   _r: PhantomData<R>,
   _w: PhantomData<W>,
 }
@@ -76,42 +78,43 @@ where
   pub async fn new(endpoints: &[String]) -> WebSocketInitResult<Self> {
     let socket = Self::connect(endpoints).await?;
     return Ok(Self {
-      socket,
+      socket: Arc::new(RwLock::new(socket)),
       endpoints: endpoints.into_iter().map(|s| s.to_string()).collect(),
-      runtime: Runtime::new()?,
+      runtime: Arc::new(Runtime::new()?),
       _r: PhantomData,
       _w: PhantomData,
     });
   }
 
-  fn close(&mut self, code: CloseCode, reason: &str) -> WSResult<()> {
-    return self.runtime.block_on(
-      self.socket.close(
+  async fn close(&mut self, code: CloseCode, reason: &str) -> WSResult<()> {
+    return self
+      .socket
+      .write()
+      .await
+      .close(
         CloseFrame {
           code,
           reason: reason.to_string().into(),
         }
         .into(),
-      ),
-    );
+      )
+      .await;
   }
 
-  fn reconnect(&mut self) -> WebSocketInitResult<()> {
-    self.socket = self
-      .runtime
-      .block_on(Self::connect(self.endpoints.as_slice()))?;
+  async fn reconnect(&mut self) -> WebSocketInitResult<()> {
+    self.socket =
+      Arc::new(RwLock::new(Self::connect(self.endpoints.as_slice()).await?));
     return Ok(());
   }
 
-  fn send(&mut self, msg: Message) -> WSResult<()> {
-    return self.runtime.block_on(async {
-      let send_result = self.socket.send(msg).await;
-      let flush_result = self.socket.flush().await;
-      send_result.and(flush_result)
-    });
+  async fn send_msg(&mut self, msg: Message) -> WSResult<()> {
+    let mut socket = self.socket.write().await;
+    let send_result = socket.send(msg).await;
+    let flush_result = socket.flush().await;
+    return send_result.and(flush_result);
   }
 
-  fn reconnect_on_error(
+  async fn reconnect_on_error(
     &mut self,
     payload: Option<WSResult<Message>>,
   ) -> WebsocketHandleResult<Option<Message>> {
@@ -125,21 +128,21 @@ where
           CloseCode::Abnormal,
           "Error while receiving payload from server.",
         );
-        self.reconnect()?;
+        self.reconnect().await?;
         return Ok(None);
       }
       None => {
         error!("Received close payload from stream. Re-Connecting...");
         let _ = self
           .close(CloseCode::Abnormal, "Received close payload from stream.");
-        self.reconnect()?;
+        self.reconnect().await?;
         return Ok(None);
       }
       Some(Ok(msg)) => return Ok(Some(msg)),
     }
   }
 
-  fn handle_message(
+  async fn handle_message(
     &mut self,
     msg: Message,
   ) -> WebsocketMessageResult<Option<R>> {
@@ -154,7 +157,7 @@ where
         return Ok(payload.into());
       }
       Message::Ping(payload) => {
-        let _ = self.send(Message::Pong(payload))?;
+        let _ = self.send_msg(Message::Pong(payload)).await?;
         return Ok(None);
       }
       Message::Pong(msg) => {
@@ -163,7 +166,7 @@ where
       }
       Message::Close(_) => {
         error!("Disconnected. Re-Connecting...");
-        let _ = self.reconnect()?;
+        let _ = self.reconnect().await?;
         return Ok(None);
       }
       Message::Frame(frame) => {
@@ -181,35 +184,41 @@ where
 {
   type Item = R;
   fn poll_next(
-    self: Pin<&mut Self>,
+    mut self: Pin<&mut Self>,
     cx: &mut Context<'_>,
   ) -> Poll<Option<Self::Item>> {
-    let me = self.get_mut();
-    match me.socket.poll_next_unpin(cx) {
-      Poll::Ready(payload) => match me.reconnect_on_error(payload) {
-        Err(e) => {
-          error!(
-            error = as_error!(e);
-            "Un-recoverable Error while handling server payload."
-          );
-          return Poll::Ready(None);
-        }
-        Ok(None) => {
-          return Poll::Pending;
-        }
-        Ok(Some(msg)) => match me.handle_message(msg) {
+    let runtime = self.runtime.clone();
+    let payload = {
+      let mut socket = runtime.block_on(self.socket.write());
+      socket.poll_next_unpin(cx)
+    };
+    match payload {
+      Poll::Ready(payload) => {
+        match runtime.block_on(self.reconnect_on_error(payload)) {
           Err(e) => {
-            error!(error = as_error!(e); "Failed to decoding the payload.");
-            return Poll::Pending;
+            error!(
+              error = as_error!(e);
+              "Un-recoverable Error while handling server payload."
+            );
+            return Poll::Ready(None);
           }
           Ok(None) => {
             return Poll::Pending;
           }
-          Ok(Some(payload)) => {
-            return Poll::Ready(Some(payload));
-          }
-        },
-      },
+          Ok(Some(msg)) => match runtime.block_on(self.handle_message(msg)) {
+            Err(e) => {
+              error!(error = as_error!(e); "Failed to decoding the payload.");
+              return Poll::Pending;
+            }
+            Ok(None) => {
+              return Poll::Pending;
+            }
+            Ok(Some(payload)) => {
+              return Poll::Ready(Some(payload));
+            }
+          },
+        }
+      }
       Poll::Pending => {
         return Poll::Pending;
       }
@@ -228,9 +237,8 @@ where
     self: Pin<&mut Self>,
     cx: &mut Context<'_>,
   ) -> Poll<Result<(), Self::Error>> {
-    let me = self.get_mut();
-    return me
-      .socket
+    let mut socket = self.runtime.block_on(self.socket.write());
+    return socket
       .poll_ready_unpin(cx)
       .map(|res| res.map_err(|err| err.into()));
   }
@@ -239,9 +247,8 @@ where
     self: Pin<&mut Self>,
     cx: &mut Context<'_>,
   ) -> Poll<Result<(), Self::Error>> {
-    let me = self.get_mut();
-    return me
-      .socket
+    let mut socket = self.runtime.block_on(self.socket.write());
+    return socket
       .poll_close_unpin(cx)
       .map(|res| res.map_err(|err| err.into()));
   }
@@ -250,17 +257,18 @@ where
     self: Pin<&mut Self>,
     cx: &mut Context<'_>,
   ) -> Poll<Result<(), Self::Error>> {
-    let me = self.get_mut();
-    return me
-      .socket
+    let mut socket = self.runtime.block_on(self.socket.write());
+    return socket
       .poll_flush_unpin(cx)
       .map(|res| res.map_err(|err| err.into()));
   }
 
-  fn start_send(self: Pin<&mut Self>, item: W) -> Result<(), Self::Error> {
-    let me = self.get_mut();
+  fn start_send(mut self: Pin<&mut Self>, item: W) -> Result<(), Self::Error> {
     let payload = jsonify(&item)?;
     let msg = Message::Text(payload);
-    return me.send(msg).map_err(|err| err.into());
+    let runtime = self.runtime.clone();
+    return runtime
+      .block_on(self.send_msg(msg))
+      .map_err(|err| err.into());
   }
 }
