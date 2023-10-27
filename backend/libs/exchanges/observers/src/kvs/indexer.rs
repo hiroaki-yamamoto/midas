6a6b4f2282ccc55cdaf94e7f6860dd1e6af249e7
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-10-27 09:50:48 +0900

    feat(trade_observer): :sparkles: Add indexer

---
+++ backend/libs/exchanges/observers/src/kvs/indexer.rs
@@ -0,0 +1,77 @@
+use ::std::marker::PhantomData;
+use ::std::sync::Arc;
+
+use ::futures::stream::{iter, BoxStream, StreamExt};
+
+use ::errors::KVSResult;
+use ::kvs::redis::AsyncCommands;
+use ::kvs::traits::last_checked::{Get, SetOp};
+use ::rpc::entities::Exchanges;
+
+pub struct NodeIndexer<T, ExchangeTypeKVS>
+where
+  T: AsyncCommands + Send + Sync,
+  ExchangeTypeKVS: Get<T, String> + SetOp<T, String> + Send + Sync,
+{
+  exchange_type_kvs: ExchangeTypeKVS,
+  _t: PhantomData<T>,
+}
+
+impl<T, ExchangeTypeKVS> NodeIndexer<T, ExchangeTypeKVS>
+where
+  T: AsyncCommands + Send + Sync,
+  ExchangeTypeKVS: Get<T, String> + SetOp<T, String> + Send + Sync,
+{
+  pub fn new(exchange_type_kvs: ExchangeTypeKVS) -> Self {
+    return Self {
+      exchange_type_kvs,
+      _t: PhantomData,
+    };
+  }
+
+  fn chname(&self) -> Arc<String> {
+    return "node_index".to_string().into();
+  }
+
+  /// Index node ids to KVS
+  pub async fn index_node(&self, node: String) -> KVSResult<usize> {
+    return self.exchange_type_kvs.sadd(self.chname(), node).await;
+  }
+
+  /// Unindex node ids to KVS
+  pub async fn unindex_node(&self, node: String) -> KVSResult<usize> {
+    return self.exchange_type_kvs.srem(self.chname(), node).await;
+  }
+
+  pub async fn get_nodes_by_exchange(
+    &self,
+    exchange: Exchanges,
+  ) -> KVSResult<BoxStream<'_, String>> {
+    let keys: Vec<String> =
+      self.exchange_type_kvs.smembers(self.chname()).await?;
+
+    let keys = iter(keys)
+      .map(move |key| {
+        let key: Arc<String> = key.into();
+        let exchange = self.exchange_type_kvs.get(key.clone());
+        return (key, exchange);
+      })
+      .filter_map(|(key, exchange)| async {
+        let pair = exchange
+          .await
+          .map(|exchange| (key, Exchanges::from_str_name(&exchange)));
+        return pair.ok();
+      })
+      .filter_map(|(key, node_exchange)| async move {
+        return node_exchange.map(|node_exchange| (key, node_exchange));
+      })
+      .filter_map(move |(key, node_exchange)| async move {
+        if node_exchange == exchange {
+          return Some((*key).clone());
+        } else {
+          return None;
+        }
+      });
+    return Ok(keys.boxed());
+  }
+}
--- backend/libs/exchanges/observers/src/kvs/mod.rs
+++ backend/libs/exchanges/observers/src/kvs/mod.rs
@@ -1,16 +1,10 @@
 mod filter;
+mod indexer;
 
-use ::std::marker::PhantomData;
-
-use ::futures::stream::{iter, BoxStream, StreamExt};
-
-use ::errors::KVSResult;
-use ::kvs::redis::AsyncCommands;
-use ::kvs::traits::last_checked::{Get, SetOp};
 use ::kvs::{LastCheckedKVSBuilder, NormalKVSBuilder};
-use ::rpc::entities::Exchanges;
 
 pub use self::filter::NodeFilter;
+pub use self::indexer::NodeIndexer;
 
 pub const NODE_KVS_BUILDER: LastCheckedKVSBuilder<String> =
   LastCheckedKVSBuilder::new("observer_node");
@@ -20,67 +14,3 @@ pub const NODE_EXCHANGE_TYPE_KVS_BUILDER: LastCheckedKVSBuilder<String> =
 
 pub const INIT_LOCK_BUILDER: NormalKVSBuilder<String> =
   NormalKVSBuilder::<String>::new("init_lock");
-
-pub struct NodeIndexer<T, ExchangeTypeKVS>
-where
-  T: AsyncCommands + Send + Sync,
-  ExchangeTypeKVS: Get<T, String> + SetOp<T, String> + Send + Sync,
-{
-  exchange_type_kvs: ExchangeTypeKVS,
-  _t: PhantomData<T>,
-}
-
-impl<T, ExchangeTypeKVS> NodeIndexer<T, ExchangeTypeKVS>
-where
-  T: AsyncCommands + Send + Sync,
-  ExchangeTypeKVS: Get<T, String> + SetOp<T, String> + Send + Sync,
-{
-  pub fn new(exchange_type_kvs: ExchangeTypeKVS) -> Self {
-    return Self {
-      exchange_type_kvs,
-      _t: PhantomData,
-    };
-  }
-
-  /// Index node ids to KVS
-  pub async fn index_node(&self, node: String) -> KVSResult<usize> {
-    return self.exchange_type_kvs.sadd("node_index", node).await;
-  }
-
-  /// Unindex node ids to KVS
-  pub async fn unindex_node(&self, node: String) -> KVSResult<usize> {
-    return self.exchange_type_kvs.srem("node_index", node).await;
-  }
-
-  pub async fn get_nodes_by_exchange(
-    &self,
-    exchange: Exchanges,
-  ) -> KVSResult<BoxStream<'_, String>> {
-    let keys: Vec<String> =
-      self.exchange_type_kvs.smembers("node_index").await?;
-
-    let keys = iter(keys)
-      .map(move |key| {
-        let exchange_key = key.clone();
-        let exchange = self.exchange_type_kvs.get(&exchange_key);
-        return (exchange_key, exchange);
-      })
-      .filter_map(|(key, exchange)| async {
-        let pair = exchange
-          .await
-          .map(|exchange| (key, Exchanges::from_str_name(&exchange)));
-        return pair.ok();
-      })
-      .filter_map(|(key, node_exchange)| async move {
-        return node_exchange.map(|node_exchange| (key, node_exchange));
-      })
-      .filter_map(move |(key, node_exchange)| async move {
-        if node_exchange == exchange {
-          return Some(key);
-        } else {
-          return None;
-        }
-      });
-    return Ok(keys.boxed());
-  }
-}
