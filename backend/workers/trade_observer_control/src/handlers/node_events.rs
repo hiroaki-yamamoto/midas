40753e2f24a2e1251acd4279cd4e93aeabaf8c86
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-10-31 23:59:59 +0900

    fix(trade_observer_control): :adhesive_bandage: Fixed build error for node_events

--- backend/workers/trade_observer_control/src/handlers/node_events.rs
+++ backend/workers/trade_observer_control/src/handlers/node_events.rs
@@ -1,42 +1,37 @@
+use ::std::sync::Arc;
 use ::std::time::Duration;
 
 use ::futures::future::{try_join, try_join_all};
 
 use ::config::{Database, ObserverConfig};
-use ::kvs::redis::Commands;
-use ::kvs::traits::normal::Expiration;
-use ::kvs::Connection;
+use ::kvs::redis::aio::MultiplexedConnection;
+use ::kvs::traits::last_checked::Expiration;
 use ::observers::entities::{
   TradeObserverControlEvent, TradeObserverNodeEvent,
 };
-use ::observers::kvs::{ONEXTypeKVS, ObserverNodeKVS};
+use ::observers::kvs::{NODE_EXCHANGE_TYPE_KVS_BUILDER, NODE_KVS_BUILDER};
 use ::observers::pubsub::NodeControlEventPubSub;
 use ::subscribe::nats::Client as Nats;
 use ::subscribe::traits::PubSub;
 
 use crate::errors::Result as ControlResult;
 
-pub struct FromNodeEventHandler<C>
-where
-  C: Commands + Send + Sync,
-{
+pub struct FromNodeEventHandler {
   control_event: NodeControlEventPubSub,
-  node_kvs: ObserverNodeKVS<C>,
-  type_kvs: ONEXTypeKVS<C>,
+  node_kvs: Arc<dyn Expiration<Commands = MultiplexedConnection> + Send + Sync>,
+  type_kvs: Arc<dyn Expiration<Commands = MultiplexedConnection> + Send + Sync>,
 }
 
-impl<C> FromNodeEventHandler<C>
-where
-  C: Commands + Send + Sync,
-{
+impl FromNodeEventHandler {
   pub async fn new(
-    kvs_com: Connection<C>,
+    kvs_com: MultiplexedConnection,
     _: Database,
     nats: &Nats,
   ) -> ControlResult<Self> {
     let control_event = NodeControlEventPubSub::new(nats).await?;
-    let node_kvs = ObserverNodeKVS::new(kvs_com.clone().into());
-    let type_kvs = ONEXTypeKVS::new(kvs_com.clone().into());
+    let node_kvs =
+      Arc::new(NODE_EXCHANGE_TYPE_KVS_BUILDER.build(kvs_com.clone()));
+    let type_kvs = Arc::new(NODE_KVS_BUILDER.build(kvs_com.clone()));
     return Ok(Self {
       control_event,
       node_kvs,
@@ -52,13 +47,12 @@ where
     match event {
       TradeObserverNodeEvent::Ping(node_id) => {
         log::debug!("Ping from {}", node_id);
+        let node_id = Arc::new(node_id);
         try_join(
           self
             .node_kvs
-            .expire(&node_id.to_string(), Duration::from_secs(30)),
-          self
-            .type_kvs
-            .expire(&node_id.to_string(), Duration::from_secs(30)),
+            .expire(node_id.clone(), Duration::from_secs(30)),
+          self.type_kvs.expire(node_id, Duration::from_secs(30)),
         )
         .await?;
       }
