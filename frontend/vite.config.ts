f85168fadd0241f171b2b050212dc134dca38b0e
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-04 16:53:10 +0900

    Generate sourcemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const opt: UserConfig = {
+    plugins: [react()],
+  };
+  if (mode === 'staging') {
+    opt.build = {
+      sourcemap: true,
+    };
+  }
+  return opt;
 });
