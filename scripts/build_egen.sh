b41cdeb668d6b55b9f5b64f252680cc934408549
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-25 14:45:29 +0900

    Introduce egen

---
+++ scripts/build_egen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+set -e
+
+source ./curl.sh
+export CC="musl-gcc -fPIE -pie -O2"
+
+BASEDIR=`dirname $0`
+VERSION='1.0.0'
+DOWNLOAD_URL="https://github.com/hiroaki-yamamoto/egen/releases/download/v${VERSION}/egen"
+FILENAME=`basename $DOWNLOAD_URL`
+EXTRACTED_NAME=`basename ${FILENAME} .tar.gz`
+WORK_DIR=`realpath $BASEDIR`
+
+rm -rf $WORK_DIR/../deps/egen
+curl $DOWNLOAD_URL -o $WORK_DIR/$FILENAME
+chmod u+x $WORK_DIR/$FILENAME
+mkdir -p $WORK_DIR/../deps/egen/bin
+mv $WORK_DIR/$FILENAME $WORK_DIR/../deps/egen/bin
