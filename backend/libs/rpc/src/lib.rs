dae33e1a6e2b7f39e74828e0d4281e823612405f
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-11-25 14:53:19 +0900

    chore: Move backend rpc directory to the old one (protoc) and new one(egen)

--- backend/libs/rpc/src/lib.rs
+++ backend/libs/rpc/src/lib.rs
@@ -1,37 +1,14 @@
-pub mod bookticker;
-pub mod bot;
-pub mod entities;
-mod entities_impl;
-pub mod historical;
-pub mod keychain;
-pub mod rejection_handler;
-pub mod symbols;
+pub fn add(left: usize, right: usize) -> usize {
+  left + right
+}
 
-use ::std::convert::TryFrom;
-use ::std::time::{SystemTime as DateTime, SystemTimeError};
+#[cfg(test)]
+mod tests {
+  use super::*;
 
-pub mod google {
-  pub mod protobuf {
-    use std::time::Duration;
-
-    include!("./google.protobuf.rs");
-
-    impl crate::TryFrom<crate::DateTime> for Timestamp {
-      type Error = crate::SystemTimeError;
-      fn try_from(value: crate::DateTime) -> Result<Self, Self::Error> {
-        let timestamp = value.duration_since(crate::DateTime::UNIX_EPOCH)?;
-        return Ok(Self {
-          seconds: timestamp.as_secs() as i64,
-          nanos: timestamp.subsec_nanos() as i32,
-        });
-      }
-    }
-
-    impl From<Timestamp> for crate::DateTime {
-      fn from(value: Timestamp) -> Self {
-        return Self::UNIX_EPOCH
-          + Duration::new(value.seconds as u64, value.nanos as u32);
-      }
-    }
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
   }
 }
