035363d9049ae2414a9bb8ce019e58563075dcc2
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-12-01 17:55:36 +0900

    feat(frontend): :construction: Integrating date-graph

--- frontend/src/profit-graph/graph.tsx
+++ frontend/src/profit-graph/graph.tsx
@@ -1,4 +1,10 @@
-import { Root } from '@amcharts/amcharts5';
+import { Root, Legend, percent } from '@amcharts/amcharts5';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import am5themes_Dark from '@amcharts/amcharts5/themes/Dark';
+import {
+  XYChart, XYCursor,
+  DateAxis, ValueAxis
+} from '@amcharts/amcharts5/xy';
 
 import { IData } from './data.interface';
 import { IGraphLegend } from '../graph-legend.interface';
@@ -6,6 +12,7 @@ import { IGraph } from '../graph.interface';
 
 export class Graph implements IGraph {
   public data: IData[] = [];
+  public root: Root | undefined = undefined;
   private readonly legend: IGraphLegend[] = [
     {
       name: 'Realized Profit Percent',
@@ -23,5 +30,38 @@ export class Graph implements IGraph {
     this.data = data;
   }
 
-  public draw(ref: HTMLElement | null): () => void { }
+  public draw(ref: HTMLElement | null): () => void {
+    if (!ref) {
+      return this.dispose;
+    }
+    const root = Root.new(ref);
+    this.root = root;
+    root.setThemes([
+      am5themes_Animated.new(root),
+      am5themes_Dark.new(root),
+    ]);
+
+    const chart = root.container.children.push(
+      XYChart.new(root, {
+        layout: root.verticalLayout,
+        panY: true,
+        panX: true,
+        wheelX: 'panX',
+        wheelY: 'zoomX',
+        cursor: XYCursor.new(root, {}),
+      })
+    );
+    chart.get('cursor').lineY.set('visible', false);
+
+    const legend = chart.children.push(Legend.new(root, {
+      x: percent(50),
+      centerX: percent(50),
+    }));
+  }
+
+  public dispose(): void {
+    if (this.root) {
+      this.root.dispose();
+    }
+  }
 }
