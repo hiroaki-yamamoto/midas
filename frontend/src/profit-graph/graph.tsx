817fc531bf0cf06f7ebaa7c7b83a861375a69dd3
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-12-01 18:40:11 +0900

    feat(frontend): :sparkles: Integrated graph renderer from old frontend

--- frontend/src/profit-graph/graph.tsx
+++ frontend/src/profit-graph/graph.tsx
@@ -1,9 +1,11 @@
-import { Root, Legend, percent } from '@amcharts/amcharts5';
+import {
+  Root, Legend, percent, Tooltip, DataProcessor
+} from '@amcharts/amcharts5';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 import am5themes_Dark from '@amcharts/amcharts5/themes/Dark';
 import {
-  XYChart, XYCursor,
-  DateAxis, ValueAxis
+  XYChart, XYCursor, AxisRendererX, AxisRendererY,
+  DateAxis, ValueAxis, LineSeries
 } from '@amcharts/amcharts5/xy';
 
 import { IData } from './data.interface';
@@ -13,7 +15,7 @@ import { IGraph } from '../graph.interface';
 export class Graph implements IGraph {
   public data: IData[] = [];
   public root: Root | undefined = undefined;
-  private readonly legend: IGraphLegend[] = [
+  private readonly series: IGraphLegend[] = [
     {
       name: 'Realized Profit Percent',
       valueField: 'realizedPercent',
@@ -51,12 +53,58 @@ export class Graph implements IGraph {
         cursor: XYCursor.new(root, {}),
       })
     );
-    chart.get('cursor').lineY.set('visible', false);
+    const cursor = chart.get('cursor');
+    if (!cursor) {
+      console.error('Cursor is not defined');
+      return this.dispose;
+    }
+    cursor.lineY.set('visible', false);
 
     const legend = chart.children.push(Legend.new(root, {
       x: percent(50),
       centerX: percent(50),
     }));
+
+    // Create axes
+    const dateAxis = chart.xAxes.push(DateAxis.new(root, {
+      renderer: AxisRendererX.new(root, {}),
+      baseInterval: { timeUnit: 'day', count: 1 },
+      tooltip: Tooltip.new(root, {}),
+      tooltipDateFormat: 'yyyy-MM-dd'
+    }));
+
+    // Create value axis
+    const valueAxis = chart.yAxes.push(ValueAxis.new(root, {
+      renderer: AxisRendererY.new(root, {}),
+    }));
+
+    // Create line series
+    const createSeries = (
+      name: string, valueField: string, tooltip: string
+    ) => {
+      const ret = chart.series.push(LineSeries.new(root, {
+        name,
+        valueXField: 'date',
+        valueYField: valueField,
+        xAxis: dateAxis,
+        yAxis: valueAxis,
+        tooltip: Tooltip.new(root, { labelText: tooltip }),
+      }));
+      ret.data.processor = DataProcessor.new(root, {
+        dateFormat: 'yyyy-MM-dd',
+        dateFields: ['date'],
+      });
+      return ret;
+    };
+
+    this.series.forEach((series) => {
+      const { name, valueField, tooltip } = series;
+      const graphSeries = createSeries(name, valueField, tooltip);
+      graphSeries.data.setAll(this.data);
+    });
+    legend.data.setAll(chart.series.values);
+    chart.appear(1000, 100);
+    return this.dispose;
   }
 
   public dispose(): void {
