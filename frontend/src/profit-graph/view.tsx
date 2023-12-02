b9c2d21921abcdd7bc8cb4960e2678332218336f
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-12-02 13:53:27 +0900

    feat(frontend): :sparkles: integrated graph with view

--- frontend/src/profit-graph/view.tsx
+++ frontend/src/profit-graph/view.tsx
@@ -1,12 +1,14 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import { Graph } from './graph';
+import { IData } from './data.interface';
 
-export default function ProfitGraph() {
+export default function ProfitGraph(input: { data: IData[] }) {
   const ref = useRef(null);
-  // useEffect(() => {
-  //   const graph = new Graph(input.legend, input.data);
-  //   const dispose = graph.draw(ref.current);
-  //   return dispose;
-  // }, [input, ref]);
+  useEffect(() => {
+    const graph = new Graph(input.data);
+    const dispose = graph.draw(ref.current);
+    return dispose;
+  }, [input, ref]);
   return (
     <div ref={ref}></div>
   );
