3223132768c5c157ba35568c88482b936bbac174
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2023-12-29 08:00:41 +0900

    feat(notification): 🔥 Removed unused import
    btw I noticed async trait has been stabilized, but it seems to be object-unsafe...

--- backend/libs/notification/src/binance/entities/mod.rs
+++ backend/libs/notification/src/binance/entities/mod.rs
@@ -6,6 +6,3 @@ mod stream;
 
 pub(crate) use self::listen_key::{ListenKey, ListenKeyPair};
 pub use self::stream::{CastedUserStreamEvents, RawUserStreamEvents};
-pub use ::history::binance::entities::{Kline, Param, Query, TradeTime};
-pub use ::symbols::binance::entities::{ExchangeInfo, Filters, Symbol};
-pub use ::symbols::types::ListSymbolStream;
