3bcc7c939d2cfb50d0cf370636c3eef5f4ebcd6b
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-01-08 08:05:10 +0900

    feat(notification): ✨ Add ListenKeyClient::create

--- backend/libs/notification/src/binance/services/listen_key_client.rs
+++ backend/libs/notification/src/binance/services/listen_key_client.rs
@@ -6,6 +6,7 @@ use ::url::Url;
 use ::clients::binance::REST_ENDPOINTS;
 use ::errors::NotificationResult;
 use ::round_robin_client::RestClient;
+use ::keychain::IQueryStringSigner
 
 use super::super::interfaces::IListenKeyClient;
 
@@ -26,4 +27,20 @@ impl ListenKeyClient {
       RestClient::new(&url, Duration::from_secs(5), Duration::from_secs(5))?;
     return Ok(Self { cli });
   }
+}
+
+impl IListenKeyClient for ListenKeyClient {
+  async fn create(&self, api_key: Arc<APIKey>) -> UserStreamResult<ListenKey> {
+
+  }
+  async fn delete(
+    &self,
+    api_key: Arc<APIKey>,
+    listen_key: Arc<ListenKey>,
+  ) -> UserStreamResult<()>;
+  async fn extend_lifetime(
+    &self,
+    api_key: Arc<APIKey>,
+    listen_key: Arc<APIKey>,
+  ) -> UserStreamResult<()>;
 }
