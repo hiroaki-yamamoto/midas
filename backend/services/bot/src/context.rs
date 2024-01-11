d5eb525263dcdc8d032fe18222f2b3dec741f2a3
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-01-11 16:48:31 +0900

    feat(bot): ✨ Integrated context with routing

--- backend/services/bot/src/context.rs
+++ backend/services/bot/src/context.rs
@@ -1,17 +1,23 @@
 use ::std::sync::Arc;
 
 use ::mongodb::Database;
+use ::reqwest::Client;
 
-use ::bot::{interfaces::IBotRepo, services::BotRepo};
+use ::bot::{
+  interfaces::{IBotRepo, ITranspiler},
+  services::{BotRepo, Transpiler},
+};
 
 pub struct Context {
   pub bot_repo: Arc<dyn IBotRepo + Send + Sync>,
+  pub transpiler: Arc<dyn ITranspiler + Send + Sync>,
 }
 
 impl Context {
-  pub fn new(db: &Database) -> Self {
+  pub fn new(db: &Database, cli: Client, transpiler_location: &str) -> Self {
     return Self {
       bot_repo: Arc::new(BotRepo::new(db)),
+      transpiler: Arc::new(Transpiler::new(cli, transpiler_location)),
     };
   }
 }
--- backend/services/bot/src/routing.rs
+++ backend/services/bot/src/routing.rs
@@ -1,60 +1,48 @@
 use ::std::convert::TryFrom;
+use ::std::sync::Arc;
 
+use ::futures::TryFutureExt;
 use ::http::StatusCode;
-use ::mongodb::Database;
 use ::warp::filters::BoxedFilter;
-use ::warp::{Filter, Reply};
+use ::warp::{Filter, Rejection, Reply};
 
 use ::bot::entities::Bot;
-use ::bot::interfaces::IBotRepo;
-use ::bot::services::BotRepo;
-use ::bot::Transpiler;
-use ::reqwest::Client;
 use ::rpc::bot::Bot as RPCBot;
 use ::rpc::status::Status;
 
-pub fn construct(
-  db: &Database,
-  cli: Client,
-  transpiler_location: &str,
-) -> BoxedFilter<(impl Reply,)> {
-  let writer = BotRepo::new(db);
-  let t_loc: String = transpiler_location.into();
+use crate::context::Context;
+
+pub fn construct(ctx: Arc<Context>) -> BoxedFilter<(impl Reply,)> {
   let register = ::warp::post()
     .and(::warp::filters::body::json())
-    .map(move |bot: RPCBot| (bot, cli.clone(), writer.clone(), t_loc.clone()))
+    .map(move |bot: RPCBot| (bot, ctx.clone()))
     .untuple_one()
-    .and_then(
-      |bot: RPCBot,
-       cli: Client,
-       writer: BotRepo,
-       transpiler_location: String| async move {
-        let bot = Bot::try_from(bot);
-        if let Err(e) = bot {
-          let code = StatusCode::EXPECTATION_FAILED;
+    .and_then(|bot: RPCBot, ctx: Arc<Context>| async move {
+      let mut bot = Bot::try_from(bot).map_err(|e| {
+        let code = StatusCode::EXPECTATION_FAILED;
+        let status = Status::new(code.clone(), &e.to_string());
+        return ::warp::reject::custom(status);
+      })?;
+      bot = ctx
+        .transpiler
+        .transpile(bot)
+        .map_err(|e| {
+          let code = StatusCode::INTERNAL_SERVER_ERROR;
           let status = Status::new(code.clone(), &e.to_string());
-          return Err(::warp::reject::custom(status));
-        }
-        let transpiler = Transpiler::new(cli, transpiler_location);
-        let bot = transpiler.transpile(&bot.unwrap()).await;
-        let bot = match bot {
-          Err(e) => {
-            let code = StatusCode::INTERNAL_SERVER_ERROR;
-            let status = Status::new(code.clone(), &e.to_string());
-            return Err(::warp::reject::custom(status));
-          }
-          Ok(bot) => bot,
-        };
-        return match writer.save(&[&bot]).await {
-          Ok(_) => Ok(bot),
-          Err(e) => {
-            let code = StatusCode::INTERNAL_SERVER_ERROR;
-            let status = Status::new(code.clone(), &e.to_string());
-            return Err(::warp::reject::custom(status));
-          }
-        };
-      },
-    )
+          return ::warp::reject::custom(status);
+        })
+        .await?;
+      let _ = ctx
+        .bot_repo
+        .save(&[&bot])
+        .map_err(|e| {
+          let code = StatusCode::INTERNAL_SERVER_ERROR;
+          let status = Status::new(code.clone(), &e.to_string());
+          return ::warp::reject::custom(status);
+        })
+        .await?;
+      return Ok::<Bot, Rejection>(bot);
+    })
     .map(|bot: Bot| {
       let bot: RPCBot = bot.into();
       return ::warp::reply::json(&bot);
