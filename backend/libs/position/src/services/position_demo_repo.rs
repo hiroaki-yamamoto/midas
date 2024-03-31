681b576a3978232c4cd2e430a46096f595a6003c
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-04-01 07:58:28 +0900

    feat(position): ✨ Implemented get method to PositionDepoRepo

--- backend/libs/position/src/services/position_demo_repo.rs
+++ backend/libs/position/src/services/position_demo_repo.rs
@@ -1,6 +1,8 @@
 use ::async_trait::async_trait;
+use ::mongodb::bson::oid::ObjectId;
 
 use ::errors::PositionResult;
+use ::rpc::bot_mode::BotMode;
 
 use crate::entities::Position;
 use crate::interfaces::IPositionRepo;
@@ -11,5 +13,10 @@ pub struct PositionDemoRepo;
 impl IPositionRepo for PositionDemoRepo {
   async fn save(&self, position: &Position) -> PositionResult<Position> {
     return Ok(position.clone());
+  }
+  async fn get(&self, id: &ObjectId) -> PositionResult<Position> {
+    let mut pos = Position::new(ObjectId::new(), BotMode::Live, "BTCUSD");
+    pos.id = id.clone();
+    return Ok(pos);
   }
 }
