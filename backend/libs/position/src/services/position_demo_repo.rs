f422126c00dfbe3f81ad382bc459f63e507d9736
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-04-02 09:52:16 +0900

    feat(position): ✨ Implemented list_by_bot_id to PositionDemoRepo

--- backend/libs/position/src/services/position_demo_repo.rs
+++ backend/libs/position/src/services/position_demo_repo.rs
@@ -1,7 +1,7 @@
-use ::async_stream::stream;
 use ::async_trait::async_trait;
-use ::futures::StreamExt;
+use ::futures::{stream::BoxStream, StreamExt};
 use ::mongodb::bson::oid::ObjectId;
+use ::tokio_stream::iter;
 
 use ::errors::PositionResult;
 use ::rpc::bot_mode::BotMode;
@@ -11,6 +11,16 @@ use crate::entities::Position;
 use crate::interfaces::IPositionRepo;
 
 pub struct PositionDemoRepo;
+
+impl PositionDemoRepo {
+  fn generate_oid_lt(&self, oid: &ObjectId) -> ObjectId {
+    let mut ret = oid.clone();
+    while &ret <= oid {
+      ret = ObjectId::new();
+    }
+    return ret;
+  }
+}
 
 #[async_trait]
 impl IPositionRepo for PositionDemoRepo {
@@ -27,8 +37,23 @@ impl IPositionRepo for PositionDemoRepo {
     bot_id: ObjectId,
     pg: Pagination,
   ) -> PositionResult<BoxStream<'_, PositionResult<Position>>> {
-    let pos_stream = stream! {
-      for i in 0..pg.limit {}
+    let pg_id: Option<ObjectId> = pg.id.map(|id| id.parse().ok()).flatten();
+    let mut pos: Vec<Position> = Vec::new();
+    let set_id = |pos: &mut Position| {
+      pos.id = if let Some(pg_id) = pg_id {
+        self.generate_oid_lt(&pg_id)
+      } else {
+        ObjectId::new()
+      }
     };
+    for _ in 0..pg.limit {
+      let mut item = Position::new(bot_id.clone(), BotMode::Live, "BTCUSD");
+      set_id(&mut item);
+      while pos.iter().find(|p| p.id == item.id).is_some() {
+        set_id(&mut item);
+      }
+      pos.push(item);
+    }
+    return Ok(iter(pos).map(|pos| Ok(pos)).boxed());
   }
 }
