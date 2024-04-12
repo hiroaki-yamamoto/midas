c5c81e93f20bba665a40171d891801eb3004f923
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-04-12 13:35:53 +0900

    feat(position): 🚧 Implementing main entrypoint

--- backend/services/position/src/main.rs
+++ backend/services/position/src/main.rs
@@ -1,5 +1,24 @@
 mod context;
 
-fn main() {
-  println!("Hello, world!");
+use ::access_logger::log;
+use ::config::init;
+use ::csrf::{CSRFOption, CSRF};
+
+use ::rpc::exchanges::Exchanges;
+
+use crate::context::Context;
+
+#[tokio::main]
+async fn main() {
+  init(|cfg, sig, db, _, host| async move {
+    let access_logger = log();
+
+    #[cfg(debug_assertions)]
+    let ctx = Context::new(true, Exchanges::Binance, db).await;
+    #[cfg(not(debug_assertions))]
+    let ctx = Context::new(Exchanges::Binance, db).await;
+
+    let csrf = CSRF::new(CSRFOption::builder());
+  })
+  .await;
 }
