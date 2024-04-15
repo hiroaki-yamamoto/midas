0c9f7a8d7a541023c32b3a88c8fa4a1f4109cf17
Hiroaki Yamamoto <my1NGtr0d5p9fAEHZTw6id3rCGfB1mPJ@hysoftware.net>
2024-04-15 13:43:51 +0900

    feat(position): 🚧 the path param should be bot-id based, not exchange

--- backend/services/position/src/routing.rs
+++ backend/services/position/src/routing.rs
@@ -1,21 +1,46 @@
+use ::std::str::FromStr;
 use ::std::sync::Arc;
 
+use ::futures::TryFutureExt;
+use ::mongodb::bson::oid::ObjectId;
 use ::warp::filters::BoxedFilter;
 use ::warp::{Filter, Reply};
 
-use ::rpc::exchanges::Exchanges;
 use ::rpc::position_query::PositionQuery;
+use ::rpc::status::{Status, StatusCode};
 
 use crate::context::Context;
 
 pub fn construct(ctx: Arc<Context>) -> BoxedFilter<(impl Reply,)> {
+  return list_by_bot(&ctx);
+}
+
+fn list_by_bot(ctx: &Arc<Context>) -> BoxedFilter<(impl Reply,)> {
   return ::warp::get()
-    .and(Exchanges::by_param())
+    .and(::warp::path("by-bot"))
+    .and(::warp::path::param())
     .and(::warp::query())
-    .and_then(|exchange: Exchanges, qs: PositionQuery| async move {
-      return Ok((ctx.clone(), exchange, qs));
+    .and_then(|bot_id: String, qs: PositionQuery| async move {
+      let bot_id = ObjectId::from_str(&bot_id).map_err(|e| {
+        let code = StatusCode::EXPECTATION_FAILED;
+        let status = Status::new(code.clone(), &e.to_string());
+        return ::warp::reject::custom(status);
+      })?;
+      return Ok((ctx.clone(), bot_id, qs));
     })
     .untuple_one()
-    .map(|ctx: Arc<Context>, exchange: Exchanges, qs: PositionQuery| {})
+    .and_then(
+      |ctx: Arc<Context>, bot_id: ObjectId, qs: PositionQuery| async move {
+        let positions = ctx
+          .position_repo
+          .list_by_bot_id(bot_id, qs.pagination.clone())
+          .map_err(|e| {
+            let code = if StatusCode::INTERNAL_SERVER_ERROR;
+            let status = Status::new(code.clone(), &e.to_string());
+            return ::warp::reject::custom(status);
+          })
+          .await?;
+      },
+    )
     .boxed();
 }
